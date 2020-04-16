3fb80233f0e4ea1b26b40a09bf6e209a8eb5c8de
Mahesh Makani <maheshmakani@gmail.com>
2020-04-16 15:49:59 +0100

    cleanup

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { css, Global } from "@emotion/core";
 import { palette } from "@guardian/src-foundations";
-import {
-  titlepiece,
-  headline,
-  body
-} from "@guardian/src-foundations/typography";
-import { Link, Route, Switch } from "react-router-dom";
-import { Button } from "@guardian/src-button";
+import { titlepiece, body } from "@guardian/src-foundations/typography";
+import { Route, Switch } from "react-router-dom";
 import { fontFaces } from "@/client/lib/fonts";
 
 const h1 = css`
@@ -17,8 +12,8 @@ const h1 = css`
   ${titlepiece.large()}
 `;
 
-const h2 = css`
-  ${headline.medium()}
+const p = css`
+  ${body.medium()};
 `;
 
 export const Main = () => {
@@ -33,18 +28,10 @@ export const Main = () => {
         `}
       />
       <h1 css={h1}>Gateway</h1>
-      <h2 css={h2}>Prototype application</h2>
-      <Link to="/">Home</Link>
-      <Link to="/test">TEST</Link>
+
       <Switch>
         <Route exact path="/">
-          <p>A skeleton app for development into a new gateway application</p>
-          <Button onClick={() => console.log("Button event fired")}>
-            Test Button
-          </Button>
-        </Route>
-        <Route path="/test">
-          <h2 css={h2}>HELLLO!</h2>
+          <p css={p}>Gateway</p>
         </Route>
       </Switch>
     </main>
