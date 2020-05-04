445e201c69df64ef8e7b21a305931976e509d6be
Mahesh Makani <maheshmakani@gmail.com>
2020-05-04 14:01:44 +0100

    reset password error handling
    Co-authored-by: Patrick Orrell <orrell@gmail.com>

--- src/server/lib/idapi/resetPassword.ts
+++ src/server/lib/idapi/resetPassword.ts
@@ -3,11 +3,26 @@ import {
   APIAddClientAccessToken,
   APIPostOptions,
 } from '@/server/lib/APIFetch';
-import { ResetPasswordErrors } from '@/shared/model/Errors';
+import { ResetPasswordErrors, IdapiErrorMessages } from '@/shared/model/Errors';
 
 const PATH = '/pwd-reset/send-password-reset-email';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const handleError = (response: any) => {
+  if (response.status === 'error' && response.errors?.length) {
+    const error = response.errors[0];
+    const message = error?.message;
+
+    switch (message) {
+      case IdapiErrorMessages.NOT_FOUND:
+        throw ResetPasswordErrors.NO_ACCOUNT;
+      case IdapiErrorMessages.MISSING_FIELD:
+        throw ResetPasswordErrors.NO_EMAIL;
+      default:
+        break;
+    }
+  }
+
   throw ResetPasswordErrors.GENERIC;
 };
 
--- src/shared/model/Errors.ts
+++ src/shared/model/Errors.ts
@@ -1,3 +1,8 @@
+export enum IdapiErrorMessages {
+  NOT_FOUND = 'Not found',
+  MISSING_FIELD = 'Required field missing',
+}
+
 export enum ResetPasswordErrors {
   GENERIC = 'There was a problem resetting your password, please try again.',
   NO_ACCOUNT = 'There is no account for that email address, please check for typos or create an account.',
