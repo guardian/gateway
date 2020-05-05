b7f4495bb57f5623c62085ce8db787a2fb579b84
Mahesh Makani <maheshmakani@gmail.com>
2020-05-05 17:07:28 +0100

    add favicon

--- src/server/lib/renderer.ts
+++ src/server/lib/renderer.ts
@@ -3,6 +3,13 @@ import ReactDOMServer from 'react-dom/server';
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 import { Main } from '@/client/main';
+import { brandBackground } from '@guardian/src-foundations/palette';
+
+// favicon shamefully stolen from dcr
+const favicon =
+  process.env.NODE_ENV === 'production'
+    ? 'favicon-32x32.ico'
+    : 'favicon-32x32-dev-yellow.ico';
 
 export const renderer: (url: string, globalState?: GlobalState) => string = (
   url,
@@ -27,7 +34,9 @@ export const renderer: (url: string, globalState?: GlobalState) => string = (
       <head>
         <meta charset='utf-8' />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Gateway</title>
+        <meta name="theme-color" content="${brandBackground.primary}" />
+        <link rel="icon" href="https://static.guim.co.uk/images/${favicon}">
+        <title>Gateway | The Guardian</title>
       </head>
       <body style="margin:0">
         <div id="app">${react}</div>
