c7549a7c0e25eb26c80a777fc53b3b4266f51c1e
Patrick Orrell <orrell@gmail.com>
2020-10-16 15:52:49 +0100

    Add fallbacks for consent retreival errors

--- src/client/pages/ConsentsCommunication.tsx
+++ src/client/pages/ConsentsCommunication.tsx
@@ -39,51 +39,59 @@ export const ConsentsCommunicationPage = () => {
 
   const market_research_optout = consents.find(
     (consent) => consent.id === Consents.MARKET_RESEARCH,
-  ) || { consented: true };
+  );
 
   const consentsWithoutOptout = consents.filter(
     (consent) => !consent.id.includes('_optout'),
   );
 
   const label = (
-    <span css={checkboxLabel}>{market_research_optout.description}</span>
+    <span css={checkboxLabel}>{market_research_optout?.description}</span>
   );
 
   return (
     <ConsentsLayout title="Stay in touch" current={CONSENTS_PAGES.CONTACT}>
-      <h3 css={[heading, autoRow()]}>Guardian products, services & events</h3>
-      <p css={[text, autoRow()]}>
-        Stay informed and up to date with all that The Guardian has to offer.
-        From time to time we can send you information about our latest products,
-        services and events.
-      </p>
-      <div css={[communicationCardContainer, autoRow()]}>
-        {consentsWithoutOptout.map((consent) => (
-          <CommunicationCard
-            key={consent.id}
-            title={consent.name}
-            body={consent.description}
-            value={consent.id}
-            checked={!!consent.consented}
-          />
-        ))}
-      </div>
-      <h3 css={[heading, autoRow()]}>Using your data for market research</h3>
-      <p css={[text, autoRow()]}>
-        From time to time we may contact you for market research purposes
-        inviting you to complete a survey, or take part in a group discussion.
-        Normally, this invitation would be sent via email, but we may also
-        contact you by phone.
-      </p>
-      <fieldset css={[fieldset, autoRow()]}>
-        <CheckboxGroup name={market_research_optout.id}>
-          <Checkbox
-            value="consent-option"
-            label={label}
-            defaultChecked={market_research_optout.consented}
-          />
-        </CheckboxGroup>
-      </fieldset>
+      {market_research_optout && (
+        <>
+          <h3 css={[heading, autoRow()]}>
+            Guardian products, services & events
+          </h3>
+          <p css={[text, autoRow()]}>
+            Stay informed and up to date with all that The Guardian has to
+            offer. From time to time we can send you information about our
+            latest products, services and events.
+          </p>
+          <div css={[communicationCardContainer, autoRow()]}>
+            {consentsWithoutOptout.map((consent) => (
+              <CommunicationCard
+                key={consent.id}
+                title={consent.name}
+                body={consent.description}
+                value={consent.id}
+                checked={!!consent.consented}
+              />
+            ))}
+          </div>
+          <h3 css={[heading, autoRow()]}>
+            Using your data for market research
+          </h3>
+          <p css={[text, autoRow()]}>
+            From time to time we may contact you for market research purposes
+            inviting you to complete a survey, or take part in a group
+            discussion. Normally, this invitation would be sent via email, but
+            we may also contact you by phone.
+          </p>
+          <fieldset css={[fieldset, autoRow()]}>
+            <CheckboxGroup name={market_research_optout.id}>
+              <Checkbox
+                value="consent-option"
+                label={label}
+                defaultChecked={market_research_optout.consented}
+              />
+            </CheckboxGroup>
+          </fieldset>
+        </>
+      )}
     </ConsentsLayout>
   );
 };
--- src/client/pages/ConsentsData.tsx
+++ src/client/pages/ConsentsData.tsx
@@ -36,46 +36,53 @@ export const ConsentsDataPage = () => {
     (consent) => consent.id === Consents.PROFILING,
   );
 
-  const label = <span css={checkboxLabel}>{profiling_optout.description}</span>;
+  const label = (
+    <span css={checkboxLabel}>{profiling_optout?.description}</span>
+  );
 
   return (
     <ConsentsLayout title="Your data" current={CONSENTS_PAGES.YOUR_DATA}>
-      <h3 css={[heading, autoRow()]}>Our commitment to you</h3>
-      <p css={[text, autoRow()]}>
-        We think carefully about our use of personal data and use it
-        responsibly. We never share it without your permission and we have a
-        team who are dedicated to keeping any data we collect safe and secure.
-        You can find out more about how The Guardian aims to safeguard users
-        data by going to the{' '}
-        <Link
-          href={Locations.PRIVACY}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Privacy
-        </Link>{' '}
-        section of the website.
-      </p>
-      <h3 css={[heading, headingMarginSpace6, autoRow()]}>
-        Using your data for marketing analysis
-      </h3>
-      <p css={[text, autoRow()]}>
-        From time to time we may use your personal data for marketing analysis.
-        That includes looking at what products or services you have bought from
-        us and what pages you have been viewing on theguardian.com and other
-        Guardian websites (e.g. Guardian Jobs or Guardian Holidays). We do this
-        to understand your interests and preferences so that we can make our
-        marketing communication more relevant to you.
-      </p>
-      <fieldset css={[fieldset, autoRow()]}>
-        <CheckboxGroup name={profiling_optout.id}>
-          <Checkbox
-            value="consent-option"
-            label={label}
-            defaultChecked={profiling_optout.consented}
-          />
-        </CheckboxGroup>
-      </fieldset>
+      {profiling_optout && (
+        <>
+          <h3 css={[heading, autoRow()]}>Our commitment to you</h3>
+          <p css={[text, autoRow()]}>
+            We think carefully about our use of personal data and use it
+            responsibly. We never share it without your permission and we have a
+            team who are dedicated to keeping any data we collect safe and
+            secure. You can find out more about how The Guardian aims to
+            safeguard users data by going to the{' '}
+            <Link
+              href={Locations.PRIVACY}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy
+            </Link>{' '}
+            section of the website.
+          </p>
+          <h3 css={[heading, headingMarginSpace6, autoRow()]}>
+            Using your data for marketing analysis
+          </h3>
+          <p css={[text, autoRow()]}>
+            From time to time we may use your personal data for marketing
+            analysis. That includes looking at what products or services you
+            have bought from us and what pages you have been viewing on
+            theguardian.com and other Guardian websites (e.g. Guardian Jobs or
+            Guardian Holidays). We do this to understand your interests and
+            preferences so that we can make our marketing communication more
+            relevant to you.
+          </p>
+          <fieldset css={[fieldset, autoRow()]}>
+            <CheckboxGroup name={profiling_optout.id}>
+              <Checkbox
+                value="consent-option"
+                label={label}
+                defaultChecked={profiling_optout.consented}
+              />
+            </CheckboxGroup>
+          </fieldset>
+        </>
+      )}
     </ConsentsLayout>
   );
 };
