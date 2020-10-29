768a525858d13754c5e4b310eca8ed0c01bf227f
Mahesh Makani <maheshmakani@gmail.com>
2020-10-29 14:56:54 +0000

    integration test up to newsletters flow

--- cypress/integration/onboarding_flow.spec.js
+++ cypress/integration/onboarding_flow.spec.js
@@ -1,15 +1,138 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /// <reference types="cypress" />
 
+const {
+  authRedirectSignInRecentlyEmailValidated,
+} = require('../support/idapi/auth');
+const { allConsents } = require('../support/idapi/consent');
+const { verifiedUserWithNoConsent } = require('../support/idapi/user');
+const { setAuthCookies } = require('../support/idapi/cookie');
+const Onboarding = require('../support/pages/onboarding_pages');
+const {
+  allNewsletters,
+  userNewsletters,
+} = require('../support/idapi/newsletter');
+
 describe('Onboarding flow', () => {
+  const onboardingFlow = new Onboarding();
+
   beforeEach(() => {
     cy.idapiMockPurge();
   });
 
+  const getCheckboxes = () => cy.get('[type="checkbox"]');
+
+  const getOptoutCheckboxes = () => getCheckboxes().get('[name*="_optout"]');
+
+  const getOptinCheckboxes = () => getCheckboxes().not('[name*="_optout"]');
+
   context('Full flow', () => {
-    // it(
-    //   'goes through full flow, opt in all consents/newsletters, preserve returnUrl',
-    // );
+    it('goes through full flow, opt in all consents/newsletters, preserve returnUrl', () => {
+      // set these cookies manually
+      // TODO: can cypress set the automatically?
+      setAuthCookies();
+
+      // set successful auth using login middleware (/consents)
+      cy.idapiMock(200, authRedirectSignInRecentlyEmailValidated);
+
+      // set successful auth using login middleware (/consents/communication)
+      cy.idapiMock(200, authRedirectSignInRecentlyEmailValidated);
+
+      // all newsletters mock response for first page of consents flow
+      cy.idapiMock(200, allConsents);
+
+      // user newsletters mock response for first page of consents flow
+      cy.idapiMock(200, verifiedUserWithNoConsent);
+
+      // setup returnurl
+      const returnUrl = encodeURIComponent(
+        `https://www.theguardian.com/science/grrlscientist/2012/aug/07/3`,
+      );
+
+      // go to onboarding flow
+      onboardingFlow.goto({
+        query: {
+          returnUrl,
+        },
+      });
+
+      // check if we're on the consents flow, communication page
+      cy.url().should(
+        'include',
+        `${Onboarding.URL}${Onboarding.CONTENT.COMMUNICATION_PAGE}`,
+      );
+
+      // check return url exists
+      cy.url().should('include', `returnUrl=${returnUrl}`);
+
+      // get all checkboxes
+      getCheckboxes()
+        // check not checked
+        .should('not.be.checked');
+
+      // get all opt in checkboxes
+      getOptinCheckboxes()
+        // select parent (to avoid cypress element not visible error)
+        .parent()
+        // click, multiple is true as we target multiple elements
+        .click({ multiple: true });
+
+      // check opt ins are checked
+      getOptinCheckboxes().should('be.checked');
+
+      // check opt outs are not checked
+      getOptoutCheckboxes().should('not.be.checked');
+
+      // mock auth for post
+      // set successful auth using login middleware (/consents)
+      cy.idapiMock(200, authRedirectSignInRecentlyEmailValidated);
+
+      // mock patch success
+      cy.idapiMock(200);
+
+      // mock load newsletters page
+      cy.idapiMock(200, authRedirectSignInRecentlyEmailValidated);
+
+      // mock load all newsletters
+      cy.idapiMock(200, allNewsletters);
+
+      // mock load user newsletters
+      cy.idapiMock(200, userNewsletters());
+
+      // does not contain go back link
+      cy.get('a')
+        .contains(Onboarding.CONTENT.GO_BACK_BUTTON)
+        .should('not.exist');
+
+      // contains save and continue button
+      cy.get('button')
+        .contains(Onboarding.CONTENT.SAVE_CONTINUE_BUTTON)
+        // click to go to next page
+        .click();
+
+      // check if we're on the consents flow, newsletters page
+      cy.url().should(
+        'include',
+        `${Onboarding.URL}${Onboarding.CONTENT.NEWSLETTER_PAGE}`,
+      );
+
+      // check return url exists
+      cy.url().should('include', `returnUrl=${returnUrl}`);
+
+      // contain go back link
+      cy.get('a').contains(Onboarding.CONTENT.GO_BACK_BUTTON).should('exist');
+
+      // get all checkboxes
+      getCheckboxes()
+        // check not checked
+        .should('not.be.checked')
+        // select parent (to avoid cypress element not visible error)
+        .parent()
+        // click, multiple is true as we target multiple elements
+        .click({ multiple: true })
+        // check if checked ;)
+        .should('be.checked');
+    });
     // it(
     //   'goes through full flow, opt out all consents/newsletters, preserve returnUrl',
     // );
---
+++ cypress/support/pages/onboarding_pages.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+const qs = require('query-string');
+
+class Onboarding {
+  static URL = '/consents';
+
+  static CONTENT = {
+    SAVE_CONTINUE_BUTTON: 'Save and continue',
+    GO_BACK_BUTTON: 'Go back',
+    COMMUNICATION_PAGE: '/communication',
+    NEWSLETTER_PAGE: '/newsletters',
+  };
+
+  goto({ failOnStatusCode = true, query = {}, path } = {}) {
+    const querystring = qs.stringify(query);
+
+    cy.visit(
+      `${Onboarding.URL}${path ? path : ''}${
+        querystring ? `?${querystring}` : ''
+      }`,
+      {
+        failOnStatusCode,
+      },
+    );
+  }
+}
+
+module.exports = Onboarding;
