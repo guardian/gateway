499f790f4c7b23bae465e4d41aceee5b98c0c878
Patrick Orrell <orrell@gmail.com>
2020-11-11 17:33:04 +0000

    Add payload inspection to mock server

--- util/mock-server.js
+++ util/mock-server.js
@@ -1,3 +1,5 @@
+// Very simple mocking server for mimicking IDAPI
+
 /* eslint-env node */
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -14,13 +16,16 @@ const app = express();
 const responses = [];
 const permanent = [];
 
-app.use(bodyParser.json());
+let payload = {};
 
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.get('/healthcheck', (_, res) => {
   console.log('healthcheck OK');
   res.sendStatus(204);
 });
 
+// Reset all mocks, including permanent
 app.get('/mock/purge', (_, res) => {
   console.log('purging all mocks');
   responses.length = 0;
@@ -28,6 +33,7 @@ app.get('/mock/purge', (_, res) => {
   res.sendStatus(204);
 });
 
+// Push mock onto mock stack
 app.post('/mock', (req, res) => {
   responses.unshift({
     status: req.get('X-status'),
@@ -36,13 +42,24 @@ app.post('/mock', (req, res) => {
   res.sendStatus(204);
 });
 
+// Always mock supplied endpoint
 app.post('/mock/permanent', (req, res) => {
   const { path, body: payload, status = 200 } = req.body;
   permanent.push({ path, payload, status });
   res.sendStatus(204);
 });
 
+// Return last POST or PATCH'd request payload
+app.get('/mock/payload', (_, res) => {
+  res.json(payload);
+});
+
+// For any request, if permanent, return permanent mock, otherwise pop from mock stack.
 app.all('*', (req, res) => {
+  const { method } = req;
+  if (method === 'POST' || method === 'PATCH') {
+    payload = req.body;
+  }
   console.log(`Mocking: ${req.originalUrl}`);
   const permanent_redirect = permanent.find((p) => p.path === req.path);
   if (permanent_redirect) {
