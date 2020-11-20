ab8be314d5dc1728fc6c2daa187977d0532049aa
Mahesh Makani <maheshmakani@gmail.com>
2020-11-20 11:33:25 +0000

    use functional way to retrieve cookie from request

--- src/server/lib/middleware/mvtId.ts
+++ src/server/lib/middleware/mvtId.ts
@@ -4,23 +4,24 @@ import { getConfiguration } from '../configuration';
 
 const { stage } = getConfiguration();
 
+const getMvtIdFromCookies = (cookies: Request['cookies']): number => {
+  if (cookies['GU_mvt_id']) {
+    return Number(cookies['GU_mvt_id']) || 0;
+  }
+
+  if (cookies['GU_mvt_id_local'] && stage === 'DEV') {
+    return Number(cookies['GU_mvt_id_local']) || 0;
+  }
+
+  return 0;
+};
+
 export const mvtIdMiddleware = (
   req: Request,
   res: ResponseWithServerStateLocals,
   next: NextFunction,
 ) => {
-  // defaults to 0 already
-  let mvtId = res.locals.abTesting.mvtId;
-
-  if (req.cookies['GU_mvt_id']) {
-    mvtId = Number(req.cookies['GU_mvt_id']) || 0;
-  }
-
-  if (req.cookies['GU_mvt_id_local'] && stage === 'DEV') {
-    mvtId = Number(req.cookies['GU_mvt_id_local']) || 0;
-  }
-
-  res.locals.abTesting.mvtId = mvtId;
+  res.locals.abTesting.mvtId = getMvtIdFromCookies(req.cookies);
 
   next();
 };
