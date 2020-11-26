49ece3ffb16947dc19be7d20eb918026a00dae4f
Mahesh Makani <maheshmakani@gmail.com>
2020-11-26 11:37:57 +0000

    error use link

--- src/client/components/GlobalError.tsx
+++ src/client/components/GlobalError.tsx
@@ -10,12 +10,17 @@ import {
   gridRow,
   COLUMNS,
 } from '@/client/styles/Grid';
+import { Link } from '@guardian/src-link';
 
 interface GlobalErrorProps {
   error: string;
   link: ErrorLink;
   left?: boolean;
 }
+
+const textColour = css`
+  color: ${neutral[100]};
+`;
 
 const errorDiv = (addSidePadding: boolean) => css`
   padding: ${space[2]}px ${addSidePadding ? space[3] : 0}px;
@@ -28,8 +33,8 @@ const errorP = (left = false) => css`
   display: flex;
   justify-content: ${left ? 'left' : 'center'};
   text-align: left;
-  color: ${neutral[100]};
   margin: 0;
+  ${textColour}
   ${textSans.medium()}
 
   svg {
@@ -43,7 +48,11 @@ const errorP = (left = false) => css`
 `;
 
 const errorLink = css`
-  color: ${neutral[100]};
+  ${textColour}
+
+  :hover {
+    ${textColour}
+  }
 `;
 
 export const GlobalError = ({ error, link, left }: GlobalErrorProps) => {
@@ -67,9 +76,9 @@ export const GlobalError = ({ error, link, left }: GlobalErrorProps) => {
           <div>
             {error}
             &nbsp;
-            <a css={errorLink} href={link.link}>
+            <Link href={link.link} css={errorLink}>
               {link.linkText}
-            </a>
+            </Link>
           </div>
         </p>
       </div>
