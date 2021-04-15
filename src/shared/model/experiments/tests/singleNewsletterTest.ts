5f7089c887075e080e7c2c29b5cd7899e7742695
Mahesh Makani <maheshmakani@gmail.com>
2021-04-15 14:37:24 +0100

    update dependencies

--- src/shared/model/experiments/tests/singleNewsletterTest.ts
+++ src/shared/model/experiments/tests/singleNewsletterTest.ts
@@ -2,8 +2,8 @@ import { ABTest } from '@guardian/ab-core';
 
 export const singleNewsletterTest: ABTest = {
   id: 'SingleNewsletterTest', // This ID must match the Server Side AB Test
-  start: '2021-04-09',
-  expiry: '2021-05-09', // Remember that the server side test expiry can be different
+  start: '2021-04-19',
+  expiry: '2021-05-19', // Remember that the server side test expiry can be different
   author: 'mahesh.makani@theguardian.com',
   description:
     'We believe that if we show a single newsletter, vs 4, it will increase opt in to at least one newsletter. . We think it will increase by at least 7.5%',
