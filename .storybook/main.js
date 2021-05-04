5f0b4606a9958568f98c8b22f24a8a1beb84a717
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-05-04 13:54:36 +0100

    fix:Edit Storybooks webpack config to support the aliases used in the code

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +8,16 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config) => {
+		// Add the @client alias to prevent imports using it from failing
+		// Nb. __dirname is the current working directory, so .storybook in this case
+		config.resolve.alias = {
+			...config.resolve.alias,
+      '@': path.join(__dirname, '../src'),
+		};
+
+    // Return the altered config
+    return config
+  },
 }
