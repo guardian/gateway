856cbf0c113869ce95ffc35b41de5dfd613a7f86
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-05-04 17:14:54 +0100

    chore: Add NotFound story

---
+++ src/client/pages/NotFound.stories.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable functional/immutable-data */
+import React from 'react';
+import { Meta } from '@storybook/react';
+
+import { NotFound } from './NotFound';
+
+export default {
+  title: 'Pages/NotFound',
+  component: NotFound,
+} as Meta;
+
+export const Default = () => <NotFound />;
+Default.storyName = 'NotFound';
