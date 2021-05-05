7e3bf359ae9970d899b75449ae014eca94bd9779
Oliver Lloyd <email@oliverlloyd.com>
2021-05-05 17:04:42 +0100

    refactor: Drop `SignInLayout` down, keeping presentation separate from state

--- src/client/components/PasswordResetDialog.tsx
+++ src/client/components/PasswordResetDialog.tsx
@@ -9,6 +9,7 @@ import { PageBody } from '@/client/components/PageBody';
 import { PageBodyText } from '@/client/components/PageBodyText';
 import { form, textInput, button } from '@/client/styles/Shared';
 import { CsrfFormField } from '@/client/components/CsrfFormField';
+import { SignInLayout } from '@/client/layouts/SignInLayout';
 
 interface PasswordResetDialogProps {
   email?: string;
@@ -25,28 +26,30 @@ export const PasswordResetDialog = ({
   buttonText,
   queryString = '',
 }: PasswordResetDialogProps) => (
-  <PageBox>
-    <PageHeader>{headerText}</PageHeader>
-    <PageBody>
-      <PageBodyText>{bodyText}</PageBodyText>
-      <form css={form} method="post" action={`${Routes.RESET}${queryString}`}>
-        <CsrfFormField />
-        <TextInput
-          css={textInput}
-          label="Email address"
-          name="email"
-          type="email"
-          defaultValue={email}
-        />
-        <Button
-          css={button}
-          type="submit"
-          icon={<SvgArrowRightStraight />}
-          iconSide="right"
-        >
-          {buttonText}
-        </Button>
-      </form>
-    </PageBody>
-  </PageBox>
+  <SignInLayout>
+    <PageBox>
+      <PageHeader>{headerText}</PageHeader>
+      <PageBody>
+        <PageBodyText>{bodyText}</PageBodyText>
+        <form css={form} method="post" action={`${Routes.RESET}${queryString}`}>
+          <CsrfFormField />
+          <TextInput
+            css={textInput}
+            label="Email address"
+            name="email"
+            type="email"
+            defaultValue={email}
+          />
+          <Button
+            css={button}
+            type="submit"
+            icon={<SvgArrowRightStraight />}
+            iconSide="right"
+          >
+            {buttonText}
+          </Button>
+        </form>
+      </PageBody>
+    </PageBox>
+  </SignInLayout>
 );
--- src/client/pages/ResendPassword.tsx
+++ src/client/pages/ResendPassword.tsx
@@ -2,20 +2,17 @@ import React, { useContext } from 'react';
 import { ClientState } from '@/shared/model/ClientState';
 import { ClientStateContext } from '@/client/components/ClientState';
 import { PasswordResetDialog } from '@/client/components/PasswordResetDialog';
-import { SignInLayout } from '@/client/layouts/SignInLayout';
 
 export const ResendPasswordPage = () => {
   const clientState: ClientState = useContext(ClientStateContext);
   const { pageData: { email = '' } = {} } = clientState;
 
   return (
-    <SignInLayout>
-      <PasswordResetDialog
-        email={email}
-        headerText="Forgotten password link expired"
-        bodyText="Oh no! The link has expired. Enter your email address and we'll send you another link to change or set your password."
-        buttonText="Send me another"
-      />
-    </SignInLayout>
+    <PasswordResetDialog
+      email={email}
+      headerText="Forgotten password link expired"
+      bodyText="Oh no! The link has expired. Enter your email address and we'll send you another link to change or set your password."
+      buttonText="Send me another"
+    />
   );
 };
