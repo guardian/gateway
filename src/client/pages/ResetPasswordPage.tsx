696dc8f734b08680d024a39983e58a740f3af61a
Oliver Lloyd <email@oliverlloyd.com>
2021-05-06 10:16:27 +0100

    fix: Removed duplicate `SignInLayout`

--- src/client/pages/ResetPasswordPage.tsx
+++ src/client/pages/ResetPasswordPage.tsx
@@ -3,7 +3,6 @@ import { ClientState } from '@/shared/model/ClientState';
 import { ClientStateContext } from '@/client/components/ClientState';
 import { ResetPassword } from '@/client/components/ResetPassword';
 import { useLocation } from 'react-router-dom';
-import { SignInLayout } from '@/client/layouts/SignInLayout';
 
 export const ResetPasswordPage = () => {
   const { search } = useLocation();
@@ -11,14 +10,12 @@ export const ResetPasswordPage = () => {
   const { pageData: { email = '' } = {} } = clientState;
 
   return (
-    <SignInLayout>
-      <ResetPassword
-        email={email}
-        headerText="Forgotten password"
-        bodyText="Forgotten or need to set your password? We will email you a link to change or set it."
-        buttonText="Reset Password"
-        queryString={search}
-      />
-    </SignInLayout>
+    <ResetPassword
+      email={email}
+      headerText="Forgotten password"
+      bodyText="Forgotten or need to set your password? We will email you a link to change or set it."
+      buttonText="Reset Password"
+      queryString={search}
+    />
   );
 };
