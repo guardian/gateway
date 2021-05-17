74c7eef51e40b7ef2ab10b7b1dee0b12c43ff686
Oliver Lloyd <email@oliverlloyd.com>
2021-05-17 11:46:02 +0100

    feat: Start to add elements to the Register page

--- src/client/pages/Registration.tsx
+++ src/client/pages/Registration.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { PageHeader } from '@/client/components/PageHeader';
+import { TextInput } from '@guardian/src-text-input';
+import { Button } from '@guardian/src-button';
 import { PageBox } from '@/client/components/PageBox';
 import { PageBody } from '@/client/components/PageBody';
-import { PageBodyText } from '@/client/components/PageBodyText';
 import { SignInLayout } from '@/client/layouts/SignInLayout';
+import { button, form, textInput } from '@/client/styles/Shared';
+import { CsrfFormField } from '@/client/components/CsrfFormField';
+import { Divider } from '../components/Divider';
 
 export const Registration = () => (
   <SignInLayout>
     <PageBox>
-      <PageHeader>Register</PageHeader>
       <PageBody>
-        <PageBodyText>Body text</PageBodyText>
+        <form css={form} method="post">
+          <CsrfFormField />
+          <TextInput css={textInput} label="Email" name="email" type="email" />
+          <Button css={button} type="submit">
+            Register
+          </Button>
+        </form>
       </PageBody>
+      <Divider size="full" spaceAbove="tight" displayText="or register with" />
+      <p>TODO: Social sign in buttons</p>
+      <p>TODO: Terms</p>
     </PageBox>
   </SignInLayout>
 );
