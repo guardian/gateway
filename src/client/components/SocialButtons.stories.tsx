6728e29e25bc512d7e258d556077734dab0b206e
Oliver Lloyd <email@oliverlloyd.com>
2021-05-19 09:33:32 +0100

    feat: Added `SocialButtons` component

---
+++ src/client/components/SocialButtons.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+
+import { SocialButtons } from './SocialButtons';
+
+export default {
+  title: 'Components/SocialButtons',
+  component: SocialButtons,
+} as Meta;
+
+export const Default = () => <SocialButtons />;
+Default.storyName = 'with defaults';
