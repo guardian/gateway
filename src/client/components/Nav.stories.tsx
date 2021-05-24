823af2d0b9ec10e72c7d43bf9982b04b9bd170a6
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-05-24 14:56:59 +0100

    feat: Added Nav component

---
+++ src/client/components/Nav.stories.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+
+import { Header } from './Header';
+import { Nav } from './Nav';
+
+export default {
+  title: 'Components/Nav',
+  component: Nav,
+  parameters: { layout: 'fullscreen' },
+} as Meta;
+
+export const Register = () => (
+  <>
+    <Header />
+    <Nav
+      tabs={[
+        {
+          displayText: 'Sign in',
+          linkTo: '',
+          isActive: false,
+        },
+        {
+          displayText: 'Register',
+          linkTo: '',
+          isActive: true,
+        },
+      ]}
+    />
+  </>
+);
+Register.storyName = 'with register active';
+
+export const SignIn = () => (
+  <>
+    <Header />
+    <Nav
+      tabs={[
+        {
+          displayText: 'Sign in',
+          linkTo: '',
+          isActive: true,
+        },
+        {
+          displayText: 'Register',
+          linkTo: '',
+          isActive: false,
+        },
+      ]}
+    />
+  </>
+);
+SignIn.storyName = 'with signin active';
+
+export const Wide = () => (
+  <>
+    <Header />
+    <Nav
+      tabs={[
+        {
+          displayText: 'Sign in',
+          linkTo: '',
+          isActive: true,
+        },
+        {
+          displayText: 'Register',
+          linkTo: '',
+          isActive: false,
+        },
+      ]}
+    />
+  </>
+);
+Wide.storyName = 'at wide breakpoint';
+Wide.parameters = {
+  viewport: {
+    defaultViewport: 'WIDE',
+  },
+};
+
+export const NoBreakpoint = () => (
+  <>
+    <Header />
+    <Nav
+      tabs={[
+        {
+          displayText: 'Sign in',
+          linkTo: '',
+          isActive: true,
+        },
+        {
+          displayText: 'Register',
+          linkTo: '',
+          isActive: false,
+        },
+      ]}
+    />
+  </>
+);
+NoBreakpoint.storyName = 'at full width';
+NoBreakpoint.parameters = {
+  viewport: {
+    defaultViewport: 'does_not_exist',
+  },
+};
