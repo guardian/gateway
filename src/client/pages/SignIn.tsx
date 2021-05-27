af0a7fdaa8c1474147db2232499171705b2d3bb1
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-05-27 09:07:44 +0100

    feat: Added Signin page visual elements

--- src/client/pages/SignIn.tsx
+++ src/client/pages/SignIn.tsx
@@ -1,20 +1,75 @@
 import React from 'react';
-import { PageHeader } from '@/client/components/PageHeader';
+import { css } from '@emotion/react';
 import { PageBox } from '@/client/components/PageBox';
 import { PageBody } from '@/client/components/PageBody';
-import { PageBodyText } from '@/client/components/PageBodyText';
 import { Main } from '@/client/layouts/Main';
 import { Header } from '@/client/components/Header';
 import { Footer } from '@/client/components/Footer';
 
+import { TextInput } from '@guardian/src-text-input';
+import { Button } from '@guardian/src-button';
+import { Routes } from '@/shared/model/Routes';
+import { PageTitle } from '@/shared/model/PageTitle';
+import { CsrfFormField } from '@/client/components/CsrfFormField';
+import { Divider } from '@/client/components/Divider';
+import { Terms } from '@/client/components/Terms';
+import { SocialButtons } from '@/client/components/SocialButtons';
+import { button, form, textInput } from '@/client/styles/Shared';
+import { Link } from '@guardian/src-link';
+import { textSans } from '@guardian/src-foundations/typography';
+
+const Links = ({ children }: { children: React.ReactNode }) => (
+  <p
+    css={css`
+      ${textSans.medium()}
+      margin-top: 0;
+    `}
+  >
+    {children}
+  </p>
+);
+
 export const SignIn = () => (
   <>
     <Header />
-    <Main subTitle="Sign in">
+    <Main subTitle={PageTitle.SIGN_IN}>
       <PageBox>
-        <PageHeader>Sign in</PageHeader>
         <PageBody>
-          <PageBodyText>Body text</PageBodyText>
+          <form css={form} method="post" action={`${Routes.SIGN_IN}`}>
+            <CsrfFormField />
+            <TextInput
+              css={textInput}
+              label="Email"
+              name="email"
+              type="email"
+            />
+            <TextInput
+              css={textInput}
+              label="Password"
+              name="password"
+              type="password"
+            />
+            <Links>
+              <Link subdued={true} href="/reset">
+                Reset password
+              </Link>{' '}
+              or{' '}
+              <Link subdued={true} href="/">
+                email me a link to sign in
+              </Link>
+            </Links>
+            <Button css={button} type="submit">
+              Sign in
+            </Button>
+          </form>
+          <Divider
+            size="full"
+            spaceAbove="loose"
+            displayText="or continue with"
+          />
+          <SocialButtons />
+          <Divider size="full" spaceAbove="tight" />
+          <Terms />
         </PageBody>
       </PageBox>
     </Main>
