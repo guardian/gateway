698156c88f346a2ddc2e9256553bae11f82b286d
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-06-14 12:44:45 +0100

    refactor: Remove unused div

--- src/client/components/ConsentsHeader.tsx
+++ src/client/components/ConsentsHeader.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { brand } from '@guardian/src-foundations/palette';
-import { css } from '@emotion/react';
 import { GlobalError } from '@/client/components/GlobalError';
 import { GlobalSuccess } from '@/client/components/GlobalSuccess';
 import { Header } from '@/client/components/Header';
@@ -11,14 +9,10 @@ type Props = {
   success?: string;
 };
 
-const headerContainer = css`
-  background-color: ${brand[400]};
-`;
-
 export const ConsentsHeader = ({ error, success }: Props) => (
-  <div css={headerContainer}>
+  <>
     <Header />
     {error && <GlobalError error={error} link={getErrorLink(error)} left />}
     {success && <GlobalSuccess success={success} />}
-  </div>
+  </>
 );
