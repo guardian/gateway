15c6b1d998a97d0947763127db62f8d23dd884f5
Mahesh Makani <mahesh@makani.dev>
2021-06-29 15:50:32 +0100

    test(okta): add sign in with okta integration test

--- cypress/integration/sign_in.spec.js
+++ cypress/integration/sign_in.spec.js
@@ -14,7 +14,7 @@ describe('Sign in flow', () => {
       injectAndCheckAxe();
     });
 
-    it.only('Has no detectable a11y violations on sign in page with error', () => {
+    it('Has no detectable a11y violations on sign in page with error', () => {
       cy.visit(
         '/signin?error=error&error_description=oh no somethings gone wrong',
       );
@@ -27,36 +27,55 @@ describe('Sign in flow', () => {
   });
 
   context('Signing in', () => {
-    it('shows an error message when sign in fails', function () {
-      cy.visit('/signin?returnUrl=https%3A%2F%2Fwww.theguardian.com%2Fabout');
-      cy.get('input[name="email"]').type('example@example.com');
-      cy.get('input[name="password"]').type('password');
-      cy.mockNext(403, {
-        status: 'error',
-        errors: [
-          {
-            message: 'Invalid email or password',
-          },
-        ],
-      });
-      cy.get('[data-cy=sign-in-button]').click();
-      cy.contains('This email and password combination is not valid');
+    it('renders global error if there is the error_description parameter in url', function () {
+      const error = 'There has been an error';
+      cy.visit(`/signin?error_description=${encodeURIComponent(error)}`);
+      cy.contains(error);
     });
 
-    it('shows a generic error message when the api error response is not known', function () {
-      cy.visit('/signin?returnUrl=https%3A%2F%2Fwww.theguardian.com%2Fabout');
+    it('shows an error message when okta authentication fails', function () {
+      cy.visit('/signin');
       cy.get('input[name="email"]').type('example@example.com');
       cy.get('input[name="password"]').type('password');
       cy.mockNext(403, {
-        status: 'error',
-        errors: [
-          {
-            message: 'Bloopity flub',
-          },
-        ],
+        errorCode: 'E0000004',
+        errorSummary: '',
+        errorLink: '',
+        errorId: '',
+        errorCauses: [],
       });
       cy.get('[data-cy=sign-in-button]').click();
-      cy.contains('There was a problem signing in, please try again');
+      cy.contains('Email and password don’t match');
+    });
+
+    it('shows a generic error message when okta rate limited', function () {
+      cy.visit('/signin');
+      cy.get('input[name="email"]').type('example@example.com');
+      cy.get('input[name="password"]').type('password');
+      cy.mockNext(403, {
+        errorCode: 'E0000047',
+        errorSummary: '',
+        errorLink: '',
+        errorId: '',
+        errorCauses: [],
+      });
+      cy.get('[data-cy=sign-in-button]').click();
+      cy.contains('There was a problem signing in, please try again.');
+    });
+
+    it('shows a generic error message when okta api response unknown', function () {
+      cy.visit('/signin');
+      cy.get('input[name="email"]').type('example@example.com');
+      cy.get('input[name="password"]').type('password');
+      cy.mockNext(403, {
+        errorCode: 'E0000147',
+        errorSummary: '',
+        errorLink: '',
+        errorId: '',
+        errorCauses: [],
+      });
+      cy.get('[data-cy=sign-in-button]').click();
+      cy.contains('There was a problem signing in, please try again.');
     });
 
     it('loads the redirectUrl upon successful authentication', function () {
@@ -64,11 +83,33 @@ describe('Sign in flow', () => {
       cy.get('input[name="email"]').type('example@example.com');
       cy.get('input[name="password"]').type('password');
       cy.mockNext(200, {
-        cookies: {
-          values: [{ key: 'key', value: 'value' }],
-          expiresAt: 'tomorrow',
+        expiresAt: '3000-01-01T00:00:00.000Z',
+        status: 'SUCCESS',
+        sessionToken: 'some-session-token',
+        _embedded: {
+          user: {
+            id: 'okta-id',
+            passwordChanged: '2020-01-01T00:00:00.000Z',
+            profile: {
+              login: 'test.man@example.com',
+              firstName: 'Test',
+              lastName: 'Man',
+              locale: 'en_GB',
+              timeZone: 'Europe/London',
+            },
+          },
         },
       });
+
+      // we can't actually check the authorization code flow
+      // so intercept the request and redirect to the guardian about page
+      cy.intercept(
+        `http://localhost:9000/oauth2/customauthserverid/v1/authorize*`,
+        (req) => {
+          req.redirect('https://theguardian.com/about');
+        },
+      );
+
       cy.get('[data-cy=sign-in-button]').click();
       cy.contains('About us');
     });
@@ -78,11 +119,33 @@ describe('Sign in flow', () => {
       cy.get('input[name="email"]').type('example@example.com');
       cy.get('input[name="password"]').type('password');
       cy.mockNext(200, {
-        cookies: {
-          values: [{ key: 'key', value: 'value' }],
-          expiresAt: 'tomorrow',
+        expiresAt: '3000-01-01T00:00:00.000Z',
+        status: 'SUCCESS',
+        sessionToken: 'some-session-token',
+        _embedded: {
+          user: {
+            id: 'okta-id',
+            passwordChanged: '2020-01-01T00:00:00.000Z',
+            profile: {
+              login: 'test.man@example.com',
+              firstName: 'Test',
+              lastName: 'Man',
+              locale: 'en_GB',
+              timeZone: 'Europe/London',
+            },
+          },
         },
       });
+
+      // we can't actually check the authorization code flow
+      // so intercept the request and redirect to the guardian homepage
+      cy.intercept(
+        `http://localhost:9000/oauth2/customauthserverid/v1/authorize*`,
+        (req) => {
+          req.redirect('https://theguardian.com/');
+        },
+      );
+
       cy.get('[data-cy=sign-in-button]').click();
       cy.contains('UK edition');
     });
