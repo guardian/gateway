651db9ac1085934f0de2528a5fbd7ecba26110bc
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-07-16 10:52:38 +0100

    refactor: Use a more Reacty declarative style for input validation

--- src/client/pages/ChangePassword.tsx
+++ src/client/pages/ChangePassword.tsx
@@ -1,13 +1,10 @@
-import React, {
-  Dispatch,
-  MutableRefObject,
-  SetStateAction,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@guardian/src-button';
 import { SvgArrowRightStraight } from '@guardian/src-icons';
+import { SvgCheckmark } from '@guardian/src-icons';
+import { SvgCross } from '@guardian/src-icons';
+import { palette } from '@guardian/src-foundations';
+import { textSans } from '@guardian/src-foundations/typography';
 import { PageBox } from '@/client/components/PageBox';
 import { PageHeader } from '@/client/components/PageHeader';
 import { PageBody } from '@/client/components/PageBody';
@@ -17,13 +14,9 @@ import { Main } from '@/client/layouts/Main';
 import { Header } from '@/client/components/Header';
 import { Footer } from '@/client/components/Footer';
 import { CsrfFormField } from '@/client/components/CsrfFormField';
-import { PasswordWeakMessage } from '@/client/components/PasswordWeakMessage';
-import { PasswordLengthMessage } from '@/client/components/PasswordLengthMessage';
 import { space } from '@guardian/src-foundations';
 import { css } from '@emotion/react';
-import { PasswordValidationResult } from '@/shared/lib/PasswordValidationResult';
 
-import { ValidationStyling } from '@/client/styles/PasswordValidationStyles';
 import sha1 from 'js-sha1';
 import { PasswordInput } from '@/client/components/PasswordInput';
 import { FieldError } from '@/shared/model/ClientState';
@@ -34,35 +27,202 @@ type ChangePasswordProps = {
   submitUrl: string;
   email: string;
   fieldErrors: FieldError[];
-  idapiBaseUrl: string;
 };
 
-type ErrorValidationResult =
-  | PasswordValidationResult.AT_LEAST_8
-  | PasswordValidationResult.MAXIMUM_72
-  | PasswordValidationResult.COMMON_PASSWORD;
-
-const PasswordValidationLongMessage = {
-  [PasswordValidationResult.AT_LEAST_8]: ChangePasswordErrors.AT_LEAST_8,
-  [PasswordValidationResult.MAXIMUM_72]: ChangePasswordErrors.MAXIMUM_72,
-  [PasswordValidationResult.COMMON_PASSWORD]:
-    ChangePasswordErrors.COMMON_PASSWORD,
-};
-
-/* eslint-disable functional/immutable-data */
-// explicit use of mutable ref
-// useRefState is used to provide access to up-to-date component state inside promises
-function useRefState<T>(
-  initial: T,
-): [T, Dispatch<SetStateAction<T>>, MutableRefObject<T>] {
-  const [state, setState] = useState(initial);
-  const stateRef = useRef(state);
-  useEffect(() => {
-    stateRef.current = state;
-  }, [state]);
-  return [state, setState, stateRef];
+enum Validation {
+  SUCCESS,
+  FAILURE,
+  ERROR,
 }
-/* eslint-enable functional/immutable-data */
+
+const ValidationSymbol = ({ result }: { result: Validation }) => {
+  const baseStyles = css`
+    display: inline-block;
+    position: relative;
+    top: 3px;
+    svg {
+      width: 16px;
+      height: 16px;
+    }
+  `;
+
+  const successStyles = css`
+    svg {
+      color: ${palette.success['400']};
+    }
+  `;
+
+  const errorStyles = css`
+    svg {
+      color: ${palette.error['400']};
+    }
+  `;
+
+  const failureStyles = css`
+    svg {
+      color: ${palette.neutral['7']};
+    }
+  `;
+
+  switch (result) {
+    case Validation.SUCCESS: {
+      return (
+        <div css={[baseStyles, successStyles]}>
+          <SvgCheckmark />
+        </div>
+      );
+    }
+    case Validation.ERROR: {
+      return (
+        <div css={[baseStyles, errorStyles]}>
+          <SvgCross />
+        </div>
+      );
+    }
+    case Validation.FAILURE:
+    default: {
+      return (
+        <div css={[baseStyles, failureStyles]}>
+          <SvgCross />
+        </div>
+      );
+    }
+  }
+};
+
+const baseMessageStyles = css`
+  ${textSans.small()}
+  margin-bottom: 4px;
+  margin-left: 3px;
+  display: inline-block;
+`;
+
+const validationStyles = (result: Validation) => {
+  let color = palette.neutral['7'];
+
+  if (result === Validation.SUCCESS) {
+    color = palette.success['400'];
+  } else if (result === Validation.ERROR) {
+    color = palette.error['400'];
+  }
+
+  return css`
+    ${textSans.small()}
+    margin-bottom: 4px;
+    margin-left: 3px;
+    display: inline-block;
+    color: ${color};
+  `;
+};
+
+const TooLong = ({ error }: { error?: string }) => {
+  return (
+    <div>
+      <ValidationSymbol
+        result={error ? Validation.ERROR : Validation.FAILURE}
+      />
+      <div
+        css={[
+          baseMessageStyles,
+          error
+            ? css`
+                color: ${palette.error['400']};
+              `
+            : css`
+                color: ${palette.neutral['7']};
+              `,
+        ]}
+      >
+        {ChangePasswordErrors.MAXIMUM_72_SHORT}
+      </div>
+    </div>
+  );
+};
+
+const TooShort = ({ error }: { error?: string }) => {
+  return (
+    <div>
+      <ValidationSymbol
+        result={error ? Validation.ERROR : Validation.FAILURE}
+      />
+      <div
+        css={[
+          baseMessageStyles,
+          error
+            ? css`
+                color: ${palette.error['400']};
+              `
+            : css`
+                color: ${palette.neutral['7']};
+              `,
+        ]}
+      >
+        {ChangePasswordErrors.AT_LEAST_8_SHORT}
+      </div>
+    </div>
+  );
+};
+
+const Valid = () => {
+  return (
+    <div>
+      <ValidationSymbol result={Validation.SUCCESS} />
+      <div css={validationStyles(Validation.SUCCESS)}>Success</div>
+    </div>
+  );
+};
+
+const Checking = () => {
+  return (
+    <div>
+      <div css={validationStyles(Validation.FAILURE)}>Checking...</div>
+    </div>
+  );
+};
+
+const Weak = () => {
+  const smallFont = css`
+    ${textSans.small()}
+    color: ${palette.neutral['7']};
+  `;
+
+  const redText = css`
+    color: ${palette.error['400']};
+  `;
+
+  return (
+    <div css={smallFont}>
+      <span css={redText}>Weak password:</span>{' '}
+      {ChangePasswordErrors.COMMON_PASSWORD_SHORT}
+    </div>
+  );
+};
+
+const ValidationMessage = ({
+  error,
+  isWeak,
+  isTooShort,
+  isTooLong,
+  isValid,
+}: {
+  error?: string;
+  isWeak: boolean;
+  isTooShort: boolean;
+  isTooLong: boolean;
+  isValid: boolean;
+}) => {
+  if (isValid) {
+    return <Valid />;
+  } else if (isTooShort) {
+    return <TooShort error={error} />;
+  } else if (isTooLong) {
+    return <TooLong error={error} />;
+  } else if (isWeak) {
+    return <Weak />;
+  } else {
+    return <Checking />;
+  }
+};
 
 const isBreached = AwesomeDebouncePromise(
   (password: string): Promise<boolean> => {
@@ -86,154 +246,45 @@ const isBreached = AwesomeDebouncePromise(
   300,
 );
 
-const validatePassword = async (
-  idapiBaseUrl: string,
-  password: string,
-): Promise<PasswordValidationResult> => {
-  if (password.length < 8) {
-    return PasswordValidationResult.AT_LEAST_8;
-  }
-
-  if (password.length > 72) {
-    return PasswordValidationResult.MAXIMUM_72;
-  }
-
-  if (await isBreached(password)) {
-    return PasswordValidationResult.COMMON_PASSWORD;
-  }
-
-  return PasswordValidationResult.VALID_PASSWORD;
-};
-
-const deriveLengthValidationStyle = (
-  validationResult: PasswordValidationResult,
-  redError: PasswordValidationResult | undefined,
-) => {
-  if (validationResult === PasswordValidationResult.VALID_PASSWORD) {
-    return ValidationStyling.SUCCESS;
-  }
-  if (redError !== undefined) {
-    return ValidationStyling.ERROR;
-  }
-  return ValidationStyling.FAILURE;
-};
-
-const updateComponentStateAfterValidation = (
-  validationResult: PasswordValidationResult,
-  password: string,
-  passwordCurrently: MutableRefObject<string>,
-  setRedError: Dispatch<SetStateAction<ErrorValidationResult | undefined>>,
-  redErrorCurrently: MutableRefObject<ErrorValidationResult | undefined>,
-  setLastLengthError: Dispatch<
-    SetStateAction<
-      PasswordValidationResult.AT_LEAST_8 | PasswordValidationResult.MAXIMUM_72
-    >
-  >,
-  setValidationResult: Dispatch<SetStateAction<PasswordValidationResult>>,
-) => {
-  // return early if the password changed before validation completed
-  if (password !== passwordCurrently.current) return;
-
-  // clear the red error if it's solved (common password does not get solved by going beneath 8 characters)
-  if (redErrorCurrently.current !== undefined) {
-    if (
-      validationResult === PasswordValidationResult.VALID_PASSWORD ||
-      (redErrorCurrently.current !== PasswordValidationResult.COMMON_PASSWORD &&
-        validationResult === PasswordValidationResult.COMMON_PASSWORD)
-    ) {
-      setRedError(undefined);
-    }
-  }
-
-  // set the last length error - when the user solves 'at least 8' or 'max 72' we show the length requirement in green
-  if (
-    validationResult === PasswordValidationResult.AT_LEAST_8 ||
-    validationResult === PasswordValidationResult.MAXIMUM_72
-  ) {
-    setLastLengthError(validationResult);
-  }
-
-  setValidationResult(validationResult);
-};
-
-// The UX requirements for password validation are subtly complex
-// see Reset password flow: https://www.figma.com/file/HrS0AJGKiUzV83dHr1UZP3/Reset-password-screen?node-id=160%3A260
-const usePasswordValidationHooks = (idapiBaseUrl: string) => {
-  // useRefState makes the up-to-date password accessible in the breachPasswordCheck promise handler
-  const [password, setPassword, passwordCurrently] = useRefState('');
-
-  // the latest validation result
-  const [validationResult, setValidationResult] =
-    useState<PasswordValidationResult>(PasswordValidationResult.AT_LEAST_8);
-
-  // redError is used for the first password input box
-  // errors go red color if the user has selected the confirm password input while the password criteria is not satisfied - or if the user submits the form and there are errors present
-  // A user has to solve the problem indicated by the red error to make it go away
-  // The red error is not necessarily the latest validation result - e.g. common password does not get solved by going beneath 8 characters
-  const [redError, setRedError, redErrorCurrently] = useRefState<
-    ErrorValidationResult | undefined
-  >(undefined);
-
-  // store last length tick/cross error since we want to show it in green after it has been corrected (it can show either password too long, or too short)
-  const [lastLengthError, setLastLengthError] = useState<
-    PasswordValidationResult.AT_LEAST_8 | PasswordValidationResult.MAXIMUM_72
-  >(PasswordValidationResult.AT_LEAST_8);
-
-  useEffect(() => {
-    validatePassword(idapiBaseUrl, password).then((validationResult) => {
-      updateComponentStateAfterValidation(
-        validationResult,
-        password,
-        passwordCurrently,
-        setRedError,
-        redErrorCurrently,
-        setLastLengthError,
-        setValidationResult,
-      );
-    });
-  }, [password]);
-
-  // this is the style for the length validation check - displayed in green, black or red
-  const lengthValidationStyle = deriveLengthValidationStyle(
-    validationResult,
-    redError,
-  );
-
-  const isCommonPassword =
-    validationResult === PasswordValidationResult.COMMON_PASSWORD;
-
-  // red error message for first password box
-  const longRedErrorMessage =
-    redError !== undefined
-      ? PasswordValidationLongMessage[redError]
-      : undefined;
-
-  return {
-    setPassword,
-    isCommonPassword,
-    lastLengthError,
-    lengthValidationStyle,
-    longRedErrorMessage,
-    setRedError,
-    validationResult,
-  };
-};
-
 export const ChangePassword = ({
   submitUrl,
   email,
   fieldErrors,
-  idapiBaseUrl,
 }: ChangePasswordProps) => {
-  const {
-    setPassword,
-    isCommonPassword,
-    lastLengthError,
-    lengthValidationStyle,
-    longRedErrorMessage,
-    setRedError,
-    validationResult,
-  } = usePasswordValidationHooks(idapiBaseUrl);
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string | undefined>(
+    fieldErrors.find((fieldError) => fieldError.field === 'password')?.message,
+  );
+  const [isWeak, setIsWeak] = useState<boolean>(false);
+  const [isTooShort, setIsTooShort] = useState<boolean>(true);
+  const [isTooLong, setIsTooLong] = useState<boolean>(false);
+  const [isValid, setIsValid] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Typing anything clears the big red error, falling back to the dynamic validation message
+    setError(undefined);
+    setIsTooShort(password.length < 8);
+    setIsTooLong(password.length > 72);
+
+    if (password.length >= 8 && password.length <= 72) {
+      // Only make api calls to check if breached when length rules are not broken
+      isBreached(password).then((breached) => {
+        if (breached) {
+          setIsWeak(true);
+          // Password is breached so not valid ❌
+          setIsValid(false);
+        } else {
+          setIsWeak(false);
+          // Password is not breached and is an acceptable length so it is valid ✔
+          setIsValid(true);
+        }
+      });
+    } else {
+      setIsWeak(false);
+      // Password is not an acceptable length so not valid ❌
+      setIsValid(false);
+    }
+  }, [password]);
 
   return (
     <>
@@ -250,11 +301,13 @@ export const ChangePassword = ({
               method="post"
               action={submitUrl}
               onSubmit={(e) => {
+                if (isTooLong || isTooShort) {
+                  setError(ChangePasswordErrors.AT_LEAST_8_SHORT);
+                } else if (isWeak) {
+                  setError(ChangePasswordErrors.COMMON_PASSWORD_SHORT);
+                }
                 // prevent the form from submitting if there are validation errors
-                if (
-                  validationResult !== PasswordValidationResult.VALID_PASSWORD
-                ) {
-                  setRedError(validationResult);
+                if (!isValid) {
                   e.preventDefault();
                 }
               }}
@@ -264,12 +317,7 @@ export const ChangePassword = ({
               <PasswordInput
                 label="New Password"
                 name="password"
-                error={
-                  longRedErrorMessage ??
-                  fieldErrors.find(
-                    (fieldError) => fieldError.field === 'password',
-                  )?.message
-                }
+                error={error}
                 onChange={(e) => {
                   setPassword(e.target.value);
                 }}
@@ -280,14 +328,13 @@ export const ChangePassword = ({
                   margin-bottom: ${space[9]}px;
                 `}
               >
-                {/* we don't render length validation success output if the password is breached */}
-                {!isCommonPassword ? (
-                  <PasswordLengthMessage
-                    validationStyling={lengthValidationStyle}
-                    lengthResult={lastLengthError}
-                  />
-                ) : null}
-                {isCommonPassword ? <PasswordWeakMessage /> : null}
+                <ValidationMessage
+                  error={error}
+                  isWeak={isWeak}
+                  isTooShort={isTooShort}
+                  isTooLong={isTooLong}
+                  isValid={isValid}
+                />
               </div>
 
               <Button
--- src/shared/lib/PasswordValidationResult.ts
+++
@@ -1,6 +0,0 @@
-export enum PasswordValidationResult {
-  AT_LEAST_8,
-  MAXIMUM_72,
-  COMMON_PASSWORD,
-  VALID_PASSWORD,
-}
