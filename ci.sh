e9eeb5c870d25c46ac1d8bc57d7ba6c3fa51468e
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-07-28 17:13:52 +0100

    refactor: Reduce the things we run in TeamCity

--- ci.sh
+++ ci.sh
@@ -8,13 +8,3 @@ yarn
 yarn lint
 yarn test
 yarn build
-yarn cypress install
-source ci.env
-CI_ENV=$(cat ci.env | tr '\n' ',')
-CI_ENV=${CI_ENV%?}
-yarn mock-server &
-yarn wait-on:mock-server
-yarn start &
-yarn wait-on:server
-yarn cypress:ci --env $CI_ENV
-
