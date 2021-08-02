19f5f01c61f8087987fea97a263402ef7dab44e5
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-08-02 14:48:24 +0100

    refactor: Move css outside of component

--- src/client/components/PasswordInput.tsx
+++ src/client/components/PasswordInput.tsx
@@ -26,6 +26,32 @@ export const isDisplayEyeOnBrowser = (browserName: string | undefined) => {
   }
 };
 
+const textInputBorderStyle = (error?: string) => css`
+  border: ${error
+    ? `4px solid ${textInputDefault.textInput.textError}`
+    : `2px solid ${textInputDefault.textInput.border}`};
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: ${height.inputMedium}px;
+  margin-bottom: ${space[3]}px;
+  padding: 0 ${space[2]}px;
+  pointer-events: none;
+`;
+
+// remove the border and shorten the width of the text input box so the text does not overlap the password eye
+const paddingRight = (isEyeDisplayedOnBrowser: boolean) => css`
+  padding-right: ${isEyeDisplayedOnBrowser ? 28 : 0}px;
+`;
+
+// we render our own border which includes the input field and eye symbol
+const noBorder = css`
+  border: none;
+  :active {
+    border: none;
+  }
+`;
+
 const EyeSymbol = ({
   isOpen,
   onClick,
@@ -45,11 +71,6 @@ const EyeSymbol = ({
     return isOpen ? <SvgEye /> : <SvgEyeStrike />;
   };
 
-  // we have to separate the click div from the symbol div, otherwise the user has to click the icon twice:
-  // once to get focus, the other to trigger the onClick event
-  //
-  // This has something to do with onFocus and onBlur events combined with re-renders on state changes preventing
-  // the first click being handled by the onClick handler
   return (
     <button
       css={style}
@@ -69,32 +90,6 @@ export const PasswordInput = ({ error, onChange }: Props) => {
     useContext(ClientStateContext);
 
   const isEyeDisplayedOnBrowser = isDisplayEyeOnBrowser(browserName);
-  const spaceForEye = isEyeDisplayedOnBrowser ? 28 : 0;
-
-  // remove the border and shorten the width of the text input box so the text does not overlap the password eye
-  // we render our own border which includes the input field and eye symbol
-  const textInputStyle = css`
-    border: none;
-    :active {
-      border: none;
-    }
-    padding-right: ${spaceForEye}px;
-  `;
-
-  const borderStyle = error
-    ? `4px solid ${textInputDefault.textInput.textError}`
-    : `2px solid ${textInputDefault.textInput.border}`;
-
-  const textInputBorderStyle = css`
-    border: ${borderStyle};
-    position: absolute;
-    bottom: 0px;
-    width: 100%;
-    height: ${height.inputMedium}px;
-    margin-bottom: ${space[3]}px;
-    padding: 0 ${space[2]}px;
-    pointer-events: none;
-  `;
 
   return (
     <div
@@ -110,7 +105,7 @@ export const PasswordInput = ({ error, onChange }: Props) => {
         supporting="Must be between 8 and 72 characters"
         css={textInput}
         type={passwordVisible ? 'text' : 'password'}
-        cssOverrides={textInputStyle}
+        cssOverrides={[noBorder, paddingRight(isEyeDisplayedOnBrowser)]}
       />
       {isEyeDisplayedOnBrowser ? (
         <EyeSymbol
@@ -121,7 +116,7 @@ export const PasswordInput = ({ error, onChange }: Props) => {
 
       {/* This div is used to show a border around the text input and password eye.
        Text input is slightly narrower so text does not overlap the show / close eye */}
-      <div css={textInputBorderStyle} />
+      <div css={textInputBorderStyle(error)} />
     </div>
   );
 };
