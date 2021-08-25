e55a230a89dca854c40bd545a546ad9a895f63e5
Oliver Barnwell <barnwelloliver@gmail.com>
2021-08-25 13:32:30 +0100

    fix: forgot to add new page component

---
+++ src/client/pages/RegistrationEmailSentPage.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { ClientState } from '@/shared/model/ClientState';
+import { ClientStateContext } from '@/client/components/ClientState';
+import { EmailSent } from '@/client/pages/EmailSent';
+
+export const RegistrationEmailSentPage = () => {
+  const clientState: ClientState = useContext(ClientStateContext);
+  const { pageData = {} } = clientState;
+  const { email } = pageData;
+
+  return (
+    <EmailSent
+      email={email}
+      subTitle={'Register'}
+      previousPage={'/signin'}
+      resendEmailAction={'/register/email-sent/resend'}
+    />
+  );
+};
