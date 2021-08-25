d7548b9e381325dc80102bac375c212b21c38170
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-08-25 15:38:34 +0100

    chore: Added end to end tests for sign in

---
+++ cypress/integration/ete/sign_in.spec.js
@@ -0,0 +1,104 @@
+/// <reference types='cypress' />
+// This test depends on this Mailslurp account already being registered
+const existing = {
+  email: '21497957-085c-4c84-93eb-e025d1dd0145@mailslurp.com',
+  password: 'existing_password',
+  inbox: '21497957-085c-4c84-93eb-e025d1dd0145',
+};
+
+describe('Sign in flow', () => {
+  it('links to the correct places', () => {
+    cy.visit('/signin');
+    cy.contains('Terms and Conditions').should(
+      'have.attr',
+      'href',
+      'https://www.theguardian.com/help/terms-of-service',
+    );
+    cy.contains('Terms of Service').should(
+      'have.attr',
+      'href',
+      'https://policies.google.com/terms',
+    );
+    cy.contains("Google's Privacy Policy").should(
+      'have.attr',
+      'href',
+      'https://policies.google.com/privacy',
+    );
+  });
+
+  it('shows a message when credentials are invalid', () => {
+    cy.visit('/signin');
+    cy.get('input[name=email]').type('invalid@doesnotexist.com');
+    cy.get('input[name=password]').type('password');
+    cy.get('[data-cy="sign-in-button"]').click();
+    cy.contains('This email and password combination is not valid');
+  });
+
+  it('correctly signs in an existing user', () => {
+    cy.visit('/signin');
+    cy.get('input[name=email]').type(existing.email);
+    cy.get('input[name=password]').type(existing.password);
+    cy.get('[data-cy="sign-in-button"]').click();
+    cy.contains('News');
+  });
+
+  it('navigates to reset password', () => {
+    cy.visit('/signin');
+    cy.contains('Reset password').click();
+    cy.contains('Forgotten password');
+  });
+
+  it('navigates to magic link', () => {
+    cy.visit('/signin');
+    cy.contains('email me a link').click();
+    cy.contains('Link to sign in');
+  });
+
+  it('navigates to registration', () => {
+    cy.visit('/signin');
+    cy.contains('Register').click();
+    cy.get('[data-cy="register-button"]').should('be.visible');
+  });
+
+  it('respects the returnUrl query param', () => {
+    cy.visit(
+      `/signin?returnUrl=${encodeURIComponent(
+        `https://www.theguardian.com/science/grrlscientist/2012/aug/07/3`,
+      )}`,
+    );
+    cy.get('input[name=email]').type(existing.email);
+    cy.get('input[name=password]').type(existing.password);
+    cy.get('[data-cy="sign-in-button"]').click();
+    cy.contains('Mystery bird');
+  });
+
+  // This functionality is still todo. Remove `skip` from this test once the returnUrl parameter is passed through
+  it.skip('redirects correctly for social sign in', () => {
+    cy.visit(
+      `/signin?returnUrl=${encodeURIComponent(
+        `https://www.theguardian.com/science/grrlscientist/2012/aug/07/3`,
+      )}`,
+    );
+    cy.get('[data-cy="google-sign-in-button"]').should(
+      'have.attr',
+      'href',
+      `https://oauth.theguardian.com/google/signin?returnUrl=${encodeURIComponent(
+        `https://www.theguardian.com/science/grrlscientist/2012/aug/07/3`,
+      )}`,
+    );
+    cy.get('[data-cy="facebook-sign-in-button"]').should(
+      'have.attr',
+      'href',
+      `https://oauth.theguardian.com/facebook/signin?returnUrl=${encodeURIComponent(
+        `https://www.theguardian.com/science/grrlscientist/2012/aug/07/3`,
+      )}`,
+    );
+    cy.get('[data-cy="apple-sign-in-button"]').should(
+      'have.attr',
+      'href',
+      `https://oauth.theguardian.com/apple/signin?returnUrl=${encodeURIComponent(
+        `https://www.theguardian.com/science/grrlscientist/2012/aug/07/3`,
+      )}`,
+    );
+  });
+});
