c254877c82a9fe7831ffad5cf2692b21a847d94e
Oliver Barnwell <barnwelloliver@gmail.com>
2021-08-26 12:29:04 +0100

    feat: add script src for thegulocal.com so local dev can hydrate the app using this uri

--- src/server/lib/middleware/helmet.ts
+++ src/server/lib/middleware/helmet.ts
@@ -20,6 +20,7 @@ enum CSP_VALID_URI {
   GUARDIAN_CONSENTS_LOGS = 'consent-logs.',
   CMP = `sourcepoint.theguardian.com gdpr-tcfv2.sp-prod.net ccpa.sp-prod.net ccpa-service.sp-prod.net ccpa-notice.sp-prod.net cdn.privacy-mgmt.com`,
   HAVEIBEENPWNED = 'https://api.pwnedpasswords.com',
+  GU_LOCAL = 'https://profile.thegulocal.com',
 }
 
 const idapiOrigin = idapiBaseUrl.replace(/https?:\/\/|\/identity-api/g, '');
@@ -29,8 +30,12 @@ const scriptSrc = [
   gaUID.hash, // google analytics id
   CSP_VALID_URI.GOOGLE_ANALYTICS,
   CSP_VALID_URI.CMP,
+  CSP_VALID_URI.GU_LOCAL,
 ];
-if (stage === 'DEV') scriptSrc.push(HELMET_OPTIONS.UNSAFE_EVAL);
+if (stage === 'DEV') {
+  scriptSrc.push(HELMET_OPTIONS.UNSAFE_EVAL);
+  scriptSrc.push(CSP_VALID_URI.GU_LOCAL);
+}
 
 const helmetConfig = {
   contentSecurityPolicy: {
