476668e7a2510b0df500f72d42cbb31e35d0b943
Oliver Barnwell <barnwelloliver@gmail.com>
2021-08-31 15:41:20 +0100

    fix: simplify email setup down to one following review

--- cypress/integration/ete/register_email_sent.spec.js
+++ cypress/integration/ete/register_email_sent.spec.js
@@ -3,14 +3,18 @@
 import { injectAndCheckAxe } from '../../support/cypress-axe';
 
 describe('Registration email sent page', () => {
-  const testUserEmail = 'test+13411@guardian.com';
-  const testUserEncryptedEmail = Buffer.from(
-    JSON.stringify(testUserEmail),
-  ).toString('base64');
+  const email = {
+    value: '7b4f97c5-79cd-4a3c-9212-cfb6db959dfc@mailslurp.com',
+    inbox: '7b4f97c5-79cd-4a3c-9212-cfb6db959dfc',
+  };
 
+  const encryptedEmail = Buffer.from(
+    JSON.stringify(email.value),
+  ).toString('base64');
+  
   context('A11y checks', () => {
     it('has no detectable a11y violations on the registration email sent page', () => {
-      cy.setCookie('PLAY_SESSION_2', testUserEncsryptedEmail, {
+      cy.setCookie('PLAY_SESSION_2', encryptedEmail, {
         log: true,
       });
       cy.visit(`/register/email-sent`);
@@ -19,32 +23,24 @@ describe('Registration email sent page', () => {
   });
 
   it('should load the page with a success banner given a valid encrypted email cookie', () => {
-    cy.setCookie('PLAY_SESSION_2', testUserEncryptedEmail, {
+    cy.setCookie('PLAY_SESSION_2', encryptedEmail, {
       log: true,
     });
     cy.visit(`/register/email-sent`);
     cy.contains('Email sent');
-    cy.contains(testUserEmail);
+    cy.contains(email.value);
     cy.contains('Resend email');
     cy.contains('Change email address');
   });
 
   // Depends on a Guest account already created using this email.
   it('should resend the email when the resend button is clicked', () => {
-    const resendTestEmail = {
-      email: '7b4f97c5-79cd-4a3c-9212-cfb6db959dfc@mailslurp.com',
-      inbox: '7b4f97c5-79cd-4a3c-9212-cfb6db959dfc',
-    };
-    const testMailslurpEncryptedEmail = Buffer.from(
-      JSON.stringify(resendTestEmail.email),
-    ).toString('base64');
-
     cy.emptyInbox(resendTestEmail.inbox).then(() => {
-      cy.setCookie('PLAY_SESSION_2', testMailslurpEncryptedEmail, {
+      cy.setCookie('PLAY_SESSION_2', encryptedEmail, {
         log: true,
       });
       cy.visit(`/register/email-sent`);
-      cy.contains(resendTestEmail.email);
+      cy.contains(email.value);
       cy.contains('Resend email').click();
       cy.waitForLatestEmail(resendTestEmail.inbox).then((email) => {
         expect(email.body).to.contain('Complete registration');
@@ -52,13 +48,13 @@ describe('Registration email sent page', () => {
         const match = email.body.match(/theguardian.com\/welcome\/([^"]*)/);
         const token = match[1];
         cy.visit(`/welcome/${token}`);
-        cy.contains('7b4f97c5-79cd-4a3c-9212-cfb6db959dfc@mailslurp.com');
+        cy.contains(email.value);
       });
     });
   });
 
   it('should navigate back to the correct page when change email is clicked', () => {
-    cy.setCookie('PLAY_SESSION_2', testUserEncryptedEmail, {
+    cy.setCookie('PLAY_SESSION_2', encryptedEmail, {
       log: true,
     });
     cy.visit(`/register/email-sent`);
