03d7014a4f4c08aa5b2765affdcb02fda4280dd3
Oliver Barnwell <barnwelloliver@gmail.com>
2021-09-07 13:56:16 +0100

    feat: track referral event in Ophan
    We use the viewId passed in from the account verification email to execute the referral event.

--- src/client/pages/Welcome.tsx
+++ src/client/pages/Welcome.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PasswordForm } from '@/client/components/PasswordForm';
 import { FieldError } from '@/shared/model/ClientState';
 import { ConsentsLayout } from '@/client/layouts/ConsentsLayout';
@@ -8,7 +8,8 @@ import {
 } from '@/client/layouts/shared/Consents';
 import { getAutoRow, gridItemColumnConsents } from '@/client/styles/Grid';
 import { CONSENTS_PAGES } from '@/client/models/ConsentsPages';
-
+import { sendOphanReferrerEvent } from '../lib/ophan';
+import { useLocation } from 'react-router-dom';
 type Props = {
   submitUrl: string;
   fieldErrors: FieldError[];
@@ -16,6 +17,19 @@ type Props = {
 
 export const Welcome = ({ submitUrl, fieldErrors }: Props) => {
   const autoRow = getAutoRow(1, gridItemColumnConsents);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(search);
+    const viewId = params.get('viewId');
+
+    if (viewId) {
+      sendOphanReferrerEvent({
+        viewId,
+      });
+    }
+  }, []);
+
   return (
     <ConsentsLayout
       title="Welcome to the Guardian"
