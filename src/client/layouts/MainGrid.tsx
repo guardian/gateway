6673c379d6536235e2bbbfe55b466fdb6ae9608f
Rebecca Thompson <becca.al.thompson@gmail.com>
2021-10-05 10:32:50 +0100

    adding error handling back in

--- src/client/layouts/MainGrid.tsx
+++ src/client/layouts/MainGrid.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { gridRow, gridItem, SpanDefinition } from '@/client/styles/Grid';
 import { css } from '@emotion/react';
 import { space } from '@guardian/src-foundations';
 import { from } from '@guardian/src-foundations/mq';
+import { ClientState } from '@/shared/model/ClientState';
+import { ClientStateContext } from '@/client/components/ClientState';
+import { SubHeader } from '@/client/components/SubHeader';
+import { GlobalError } from '@/client/components/GlobalError';
+import { getErrorLink } from '@/client/lib/ErrorLink';
+import { GlobalSuccess } from '@/client/components/GlobalSuccess';
 
 type Props = {
+  subTitle?: string;
+  successOverride?: string;
   children: React.ReactNode;
   gridSpanDefinition?: SpanDefinition;
 };
@@ -23,9 +31,22 @@ const sectionStyle = css`
   }
 `;
 
-export const MainGrid = ({ children, gridSpanDefinition }: Props) => {
+export const MainGrid = ({
+  subTitle,
+  successOverride,
+  children,
+  gridSpanDefinition,
+}: Props) => {
+  const clientState: ClientState = useContext(ClientStateContext);
+  const { globalMessage: { error, success } = {} } = clientState;
+
+  const successMessage = success || successOverride;
+
   return (
     <main css={[gridRow, mainStyle]}>
+      {subTitle && <SubHeader title={subTitle} />}
+      {error && <GlobalError error={error} link={getErrorLink(error)} />}
+      {successMessage && <GlobalSuccess success={successMessage} />}
       <section css={[sectionStyle, gridItem(gridSpanDefinition)]}>
         {children}
       </section>
