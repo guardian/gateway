202260f4b6ab7d9a80e3b707d5a637a5634536e5
Oliver Barnwell <barnwelloliver@gmail.com>
2021-10-07 13:31:48 +0100

    fix: linting

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -43,7 +43,7 @@ export const App = (props: ClientState) => {
           * {
             box-sizing: border-box;
           }
-          // Badge is hidden for Gateway, because we're using 
+          // Badge is hidden for Gateway, because we're using
           // the legal text to do this job
           .grecaptcha-badge {
             visibility: hidden;
