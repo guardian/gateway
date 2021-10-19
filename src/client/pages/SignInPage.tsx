e61944ad3783031420b0793346bbe8882e7cdff2
Rebecca Thompson <becca.al.thompson@gmail.com>
2021-10-19 12:14:06 +0100

    refactor: pass params into sign-in page to align with registration page

--- src/client/pages/SignInPage.tsx
+++ src/client/pages/SignInPage.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SignIn } from '@/client/pages/SignIn';
+import { ClientState } from '@/shared/model/ClientState';
+import { ClientStateContext } from '@/client/components/ClientState';
 
 export const SignInPage = () => {
-  return <SignIn />;
+  const clientState: ClientState = useContext(ClientStateContext);
+  const { pageData = {} } = clientState;
+  const { returnUrl, email } = pageData;
+
+  return <SignIn returnUrl={returnUrl} email={email} />;
 };
