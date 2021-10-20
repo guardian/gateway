45d2a1c78857194bbc95821dcceff46d9e8f30a1
Mahesh Makani <mahesh.makani@guardian.co.uk>
2021-10-20 16:50:11 +0100

    feat (footer): update footer to match grid using flexbox
    This updates the Footer component to match alignment against the grid layout.
    
    We're using flexbox instead of grid, as it provides more flexibility in 1D alignment, as some of the footer tabs are aligned to the middle of the grid gutter/column-gap rather than the start of the column.
    Using css-grid it's not (easily) possible to position items at the middle of the grid gap.

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -8,7 +8,7 @@ import {
 } from '@guardian/src-foundations/palette';
 import { space } from '@guardian/src-foundations';
 import { textSans } from '@guardian/src-foundations/typography';
-import { from, until } from '@guardian/src-foundations/mq';
+import { from } from '@guardian/src-foundations/mq';
 import { Link } from '@guardian/src-link';
 import { Hide } from '@guardian/src-layout';
 import locations from '@/client/lib/locations';
@@ -29,14 +29,23 @@ const ulWrapperStyles = css`
 
 const linkStyles = css`
   text-decoration: none;
-  padding-bottom: ${space[3]}px;
+  padding-bottom: 10px;
   display: block;
   line-height: 19px;
   color: ${brandText.primary};
+  font-size: 15px;
 
   :hover {
     text-decoration: underline;
     color: ${brandAlt[400]};
+  }
+
+  ${from.mobileMedium} {
+    font-size: 17px;
+  }
+
+  ${from.desktop} {
+    padding-bottom: 0;
   }
 `;
 
@@ -50,29 +59,69 @@ const columnStyles = css`
   flex: 1;
   padding-top: ${space[3]}px;
   padding-right: 0;
-  padding-bottom: 0;
+  padding-bottom: 50px;
   padding-left: 0;
   margin-top: 0;
   margin-right: ${space[3]}px;
-  margin-bottom: ${space[6]}px;
+  margin-bottom: 0px;
   margin-left: 0;
+
+  ${from.tablet} {
+    flex: 0 0 160px;
+    padding-top: 10px;
+    padding-bottom: 26px;
+    margin-right: 0;
+  }
+
+  ${from.desktop} {
+    padding-bottom: ${space[6]}px;
+    flex: 0 0 160px;
+
+    :first-of-type {
+      /* The first column on desktop
+      is slightly shorter as it ends
+      halfway (10px) into the grid
+      column gap between col 2 and 3.
+      
+      This also means that the rest of
+      the footer items line up with
+      the border in the column gap
+      and the text at the start of the
+      column;
+      */
+      flex: 0 0 150px;
+    }
+  }
 `;
 
 const leftBorderStyles = css`
-  border-left: 1px solid ${brand[600]};
   padding-left: ${space[3]}px;
-  ${from.tablet} {
-    padding-left: 20px;
+
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 21px;
+    border-left: 1px solid ${brand[600]};
+    margin-left: -${space[3]}px;
+  }
+
+  ${from.desktop} {
+    padding-left: 10px;
+
+    ::before {
+      margin-left: -10px;
+    }
   }
 `;
 
 const copyrightStyles = css`
   ${textSans.xxsmall()};
 
-  padding-top: ${space[3]}px !important;
-  padding-bottom: ${space[3]}px !important;
-  ${until.tablet} {
-    padding-top: ${space[6]}px !important;
+  padding-top: ${space[6]}px !important;
+  padding-bottom: 18px !important;
+  ${from.tablet} {
+    padding-top: ${space[3]}px !important;
   }
 `;
 
@@ -82,12 +131,17 @@ const backToTopStyles = css`
   position: absolute;
   bottom: -21px;
   right: ${space[3]}px;
+
+  ${from.tablet} {
+    padding: 0 5px;
+    right: 0;
+  }
 `;
 
 export const Footer = () => (
   <footer css={footer}>
-    {/* What you see below tablet */}
-    <Hide above="tablet">
+    {/* What you see below desktop */}
+    <Hide above="desktop">
       <Container sideBorders={true}>
         <div css={ulWrapperStyles}>
           <ul css={[ulStyles, columnStyles]}>
@@ -101,11 +155,6 @@ export const Footer = () => (
                 Contact us
               </Link>
             </li>
-            <li>
-              <Link cssOverrides={linkStyles} href={locations.COOKIE_POLICY}>
-                Cookie policy
-              </Link>
-            </li>
           </ul>
           <ul css={[ulStyles, columnStyles, leftBorderStyles]}>
             <li>
@@ -115,7 +164,7 @@ export const Footer = () => (
             </li>
             <li>
               <Link cssOverrides={linkStyles} href={locations.TERMS}>
-                Terms and Conditions
+                Terms &amp; Conditions
               </Link>
             </li>
           </ul>
@@ -125,8 +174,8 @@ export const Footer = () => (
         </div>
       </Container>
     </Hide>
-    {/* What you see above tablet */}
-    <Hide below="tablet">
+    {/* What you see above desktop */}
+    <Hide below="desktop">
       <Container sideBorders={true}>
         <div css={ulWrapperStyles}>
           <ul css={[ulStyles, columnStyles]}>
@@ -153,14 +202,7 @@ export const Footer = () => (
           <ul css={[ulStyles, columnStyles, leftBorderStyles]}>
             <li>
               <Link cssOverrides={linkStyles} href={locations.TERMS}>
-                Terms and Conditions
-              </Link>
-            </li>
-          </ul>
-          <ul css={[ulStyles, columnStyles, leftBorderStyles]}>
-            <li>
-              <Link cssOverrides={linkStyles} href={locations.COOKIE_POLICY}>
-                Cookie policy
+                Terms &amp; Conditions
               </Link>
             </li>
           </ul>
