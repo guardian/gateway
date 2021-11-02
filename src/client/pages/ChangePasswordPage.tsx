1ee493836dd026e1dfce9af51ab706a083e543b7
Mahesh Makani <mahesh@makani.dev>
2021-11-02 09:41:19 +0000

    fix: run setTimeout on first render only using useEffect

--- src/client/pages/ChangePasswordPage.tsx
+++ src/client/pages/ChangePasswordPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { ClientState } from '@/shared/model/ClientState';
 import { ClientStateContext } from '@/client/components/ClientState';
@@ -13,16 +13,18 @@ export const ChangePasswordPage = () => {
   } = clientState;
   const { token } = useParams<{ token: string }>();
 
-  // we only want this to run in the browser as window is not
-  // defined on the server
-  // and we also check that the expiry time exists so that
-  // we redirect to the session expired page
-  // if the token expires while the user is on the current page
-  if (typeof window !== 'undefined' && tokenExpiryTimestamp) {
-    setTimeout(() => {
-      window.location.replace(`${Routes.RESET}${Routes.EXPIRED}`);
-    }, tokenExpiryTimestamp - Date.now());
-  }
+  useEffect(() => {
+    // we only want this to run in the browser as window is not
+    // defined on the server
+    // and we also check that the expiry time exists so that
+    // we redirect to the session expired page
+    // if the token expires while the user is on the current page
+    if (typeof window !== 'undefined' && tokenExpiryTimestamp) {
+      setTimeout(() => {
+        window.location.replace(`${Routes.RESET}${Routes.EXPIRED}`);
+      }, tokenExpiryTimestamp - Date.now());
+    }
+  }, []);
 
   return (
     <ChangePassword
