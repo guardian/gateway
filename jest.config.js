d46e8585319d5c881ecb4b4f6b2a643f54369884
Mahesh Makani <mahesh@makani.dev>
2021-11-04 19:43:06 +0000

    ci: fix jest memory leak

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
+/* eslint-disable functional/immutable-data */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: false,
+
   moduleNameMapper: {
     '@/([^\\.]*)$': '<rootDir>/src/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/cypress/', '<rootDir>/node_modules/'],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
 };
