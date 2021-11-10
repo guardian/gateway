b6789b072b513f0d2fc5fb7d8eea5a09b2e2ead5
Mahesh Makani <mahesh@makani.dev>
2021-11-10 13:25:59 +0000

    refactor(emails): update emails to new designs

--- src/email/components/Header.tsx
+++ src/email/components/Header.tsx
@@ -4,10 +4,16 @@ import { MjmlSection, MjmlColumn, MjmlImage } from 'mjml-react';
 import { brandBackground } from '@guardian/src-foundations/palette';
 
 export const Header = () => (
-  <MjmlSection background-color={brandBackground.primary} padding="0">
+  <MjmlSection
+    background-color={brandBackground.primary}
+    padding="0 12px"
+    fullWidth
+  >
     <MjmlColumn>
       <MjmlImage
-        width="200px"
+        padding="13px 0px"
+        width="144px"
+        height="46px"
         align="right"
         src="https://s3-eu-west-1.amazonaws.com/identity-public-email-assets/logo.white.med.png"
       />
--- src/email/components/Text.tsx
+++ src/email/components/Text.tsx
@@ -3,16 +3,20 @@ import React from 'react';
 import { MjmlSection, MjmlColumn, MjmlText } from 'mjml-react';
 import { background, text } from '@guardian/src-foundations/palette';
 
-type Props = { children: React.ReactNode };
+type Props = { children: React.ReactNode; noPaddingBottom?: boolean };
 
-export const Text = ({ children }: Props) => (
-  <MjmlSection background-color={background.primary} padding="0">
+export const Text = ({ children, noPaddingBottom = false }: Props) => (
+  <MjmlSection
+    background-color={background.primary}
+    padding={noPaddingBottom ? '0 12px' : '0 12px 12px 12px'}
+  >
     <MjmlColumn>
       <MjmlText
-        padding="0 48px"
-        font-size="17px"
-        line-height="22.95px"
-        fontFamily="GuardianTextSans, Guardian Text Sans Web, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif"
+        padding="0"
+        fontSize="17px"
+        lineHeight="23px"
+        letterSpacing="-2%"
+        fontFamily="Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif"
         color={text.primary}
       >
         {children}
