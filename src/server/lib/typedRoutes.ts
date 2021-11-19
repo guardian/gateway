58146066f5acee51ed16a5f37919390e39babe83
Dominic Kendrick <dom@dominickendrick.co.uk>
2021-11-19 17:35:04 +0000

    adding typedRoutes module

---
+++ src/server/lib/typedRoutes.ts
@@ -0,0 +1,16 @@
+import { RoutePaths } from '@/shared/lib/routeUtils';
+import { Router } from 'express';
+
+const router = Router();
+
+export const typedRouter = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  get: (path: RoutePaths, ...handlers: Array<any>) => {
+    return router.get(path, ...handlers);
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  post: (path: RoutePaths, ...handlers: Array<any>) => {
+    return router.get(path, ...handlers);
+  },
+  router: router,
+};
