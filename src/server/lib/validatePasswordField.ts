d9c9f2bf78f83898779456ed3e99b18dc924b0dd
Rebecca Thompson <becca.al.thompson@gmail.com>
2021-12-13 09:43:11 +0000

    fix password validation

--- src/server/lib/validatePasswordField.ts
+++ src/server/lib/validatePasswordField.ts
@@ -9,7 +9,7 @@ export const validatePasswordField = (password: string): Array<FieldError> => {
       field: 'password',
       message: ChangePasswordErrors.PASSWORD_BLANK,
     });
-  } else if (password.length <= 8 || password.length > 72) {
+  } else if (password.length < 8 || password.length > 72) {
     errors.push({
       field: 'password',
       message: ChangePasswordErrors.PASSWORD_LENGTH,
