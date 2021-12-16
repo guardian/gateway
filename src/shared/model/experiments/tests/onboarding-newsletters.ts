22f7f1845b760eab7b32be0328a4e407f65d40e7
Liam Duffy <3338808+liamness@users.noreply.github.com>
2021-12-16 16:57:55 +0000

    Acknowledging the passage of time, and other fixes

--- src/shared/model/experiments/tests/onboarding-newsletters.ts
+++ src/shared/model/experiments/tests/onboarding-newsletters.ts
@@ -7,8 +7,8 @@ export const TEST_VARIANT = 'test';
 export const onboardingNewslettersTest: ABTest = {
   id: TEST_ID,
   // TODO: change start / expiry before release
-  start: '2020-12-16',
-  expiry: '2020-12-31',
+  start: '2021-12-16',
+  expiry: '2021-12-31',
   author: 'liam.duffy.freelancer@guardian.co.uk',
   description: 'Testing alternative newsletter options in onboarding flow',
   audience: 1,
