ee437a17e72e9d90ac86ef83960914008de135df
Liam Duffy <3338808+liamness@users.noreply.github.com>
2021-12-20 13:45:10 +0000

    Fix integration test

--- cypress/integration/mocked/onboarding_flow.spec.ts
+++ cypress/integration/mocked/onboarding_flow.spec.ts
@@ -123,8 +123,8 @@ describe('Onboarding flow', () => {
       cy.lastPayloadIs([
         { id: '4137', subscribed: true },
         { id: '4147', subscribed: true },
-        { id: '4151', subscribed: true },
         { id: '4165', subscribed: true },
+        { id: '4151', subscribed: true },
       ]);
 
       cy.url().should('include', YourDataPage.URL);
