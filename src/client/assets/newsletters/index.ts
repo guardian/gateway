70ffdccce8f66117ef4099e6b66b57b300264d26
Liam Duffy <3338808+liamness@users.noreply.github.com>
2022-01-10 16:57:47 +0000

    Update newsletter images with latest from design

--- src/client/assets/newsletters/index.ts
+++ src/client/assets/newsletters/index.ts
@@ -1,6 +1,7 @@
 export { default as BOOKMARKS_IMAGE } from './bookmarks.jpg';
-export { default as DOWN_TO_EARTH_IMAGE } from './down-to-earth.jpg';
-export { default as MORNING_BRIEFING_IMAGE } from './morning-briefing.png';
+export { default as DOWN_TO_EARTH_IMAGE } from './down-to-earth.png';
+export { default as MORNING_BRIEFING_AU_IMAGE } from './morning-briefing-au.jpg';
+export { default as MORNING_BRIEFING_UK_IMAGE } from './morning-briefing-uk.jpg';
 export { default as MORNING_BRIEFING_US_IMAGE } from './morning-briefing-us.jpg';
 export { default as THE_GUIDE_IMAGE } from './the-guide.jpg';
 export { default as THE_LONG_READ_IMAGE } from './the-long-read.jpg';
