c53f181e33c29e97596e02f33f34204dae8c0e63
Mahesh Makani <mahesh@makani.dev>
2022-01-25 16:37:55 +0000

    test(cypress): add mocked tests for sign in with okta

--- cypress/integration/mocked/sign_in.spec.ts
+++ cypress/integration/mocked/sign_in.spec.ts
@@ -145,6 +145,151 @@ describe('Sign in flow', () => {
     });
   });
 
+  context.skip('Signing in - Okta (Skipped while Okta is disabled)', () => {
+    const defaultReturnUrl = 'https://m.code.dev-theguardian.com';
+
+    beforeEach(() => {
+      // set the encrypted state signInRedirect to true to render sign in page
+      // we can't mock the session check because it's a redirect to and from okta
+      cy.setEncryptedStateCookie({ signInRedirect: true });
+    });
+
+    it('renders global error if there is the error_description parameter in url', function () {
+      const error = 'There has been an error';
+      cy.visit(
+        `/signin?error_description=${encodeURIComponent(error)}&useOkta=true`,
+      );
+      cy.contains(error);
+    });
+
+    it('shows an error message when okta authentication fails', function () {
+      cy.visit('/signin?useOkta=true');
+      cy.get('input[name="email"]').type('example@example.com');
+      cy.get('input[name="password"]').type('password');
+      cy.mockNext(401, {
+        errorCode: 'E0000004',
+        errorSummary: 'errorSummary',
+        errorLink: '',
+        errorId: 'errorId',
+        errorCauses: [],
+      });
+      cy.get('[data-cy=sign-in-button]').click();
+      cy.contains("Email and password don't match");
+    });
+
+    it('shows a generic error message when okta rate limited', function () {
+      cy.visit('/signin?useOkta=true');
+      cy.get('input[name="email"]').type('example@example.com');
+      cy.get('input[name="password"]').type('password');
+      cy.mockNext(429, {
+        errorCode: 'E0000047',
+        errorSummary: 'errorSummary',
+        errorLink: '',
+        errorId: 'errorId',
+        errorCauses: [],
+      });
+      cy.get('[data-cy=sign-in-button]').click();
+      cy.contains('There was a problem signing in, please try again.');
+    });
+
+    it('shows a generic error message when okta api response unknown', function () {
+      cy.visit('/signin?useOkta=true');
+      cy.get('input[name="email"]').type('example@example.com');
+      cy.get('input[name="password"]').type('password');
+      cy.mockNext(403, {
+        errorCode: 'E0000147',
+        errorSummary: 'errorSummary',
+        errorLink: '',
+        errorId: 'errorId',
+        errorCauses: [],
+      });
+      cy.get('[data-cy=sign-in-button]').click();
+      cy.contains('There was a problem signing in, please try again.');
+    });
+
+    it('loads the redirectUrl upon successful authentication', function () {
+      cy.visit(
+        '/signin?returnUrl=https%3A%2F%2Fwww.theguardian.com%2Fabout&useOkta=true',
+      );
+      cy.get('input[name="email"]').type('example@example.com');
+      cy.get('input[name="password"]').type('password');
+      cy.mockNext(200, {
+        expiresAt: '3000-01-01T00:00:00.000Z',
+        status: 'SUCCESS',
+        sessionToken: 'some-session-token',
+        _embedded: {
+          user: {
+            id: 'okta-id',
+            passwordChanged: '2020-01-01T00:00:00.000Z',
+            profile: {
+              login: 'test.man@example.com',
+              firstName: 'Test',
+              lastName: 'Man',
+              locale: 'en_GB',
+              timeZone: 'Europe/London',
+            },
+          },
+        },
+      });
+
+      // we can't actually check the authorization code flow
+      // so intercept the request and redirect to the guardian about page
+      cy.intercept(
+        `http://localhost:9000/oauth2/${Cypress.env(
+          'OKTA_CUSTOM_OAUTH_SERVER',
+        )}/v1/authorize*`,
+        (req) => {
+          req.redirect(defaultReturnUrl);
+        },
+      ).as('authRedirect');
+
+      cy.get('[data-cy=sign-in-button]').click();
+      cy.waitFor('@authRedirect').then(() => {
+        cy.url().contains('https://theguardian.com/about');
+      });
+    });
+
+    it('redirects to the default url if no redirectUrl given', function () {
+      cy.visit('/signin');
+      cy.get('input[name="email"]').type('example@example.com');
+      cy.get('input[name="password"]').type('password');
+      cy.mockNext(200, {
+        expiresAt: '3000-01-01T00:00:00.000Z',
+        status: 'SUCCESS',
+        sessionToken: 'some-session-token',
+        _embedded: {
+          user: {
+            id: 'okta-id',
+            passwordChanged: '2020-01-01T00:00:00.000Z',
+            profile: {
+              login: 'test.man@example.com',
+              firstName: 'Test',
+              lastName: 'Man',
+              locale: 'en_GB',
+              timeZone: 'Europe/London',
+            },
+          },
+        },
+      });
+
+      // we can't actually check the authorization code flow
+      // so intercept the request and redirect to the guardian homepage
+      cy.intercept(
+        `http://localhost:9000/oauth2/${Cypress.env(
+          'OKTA_CUSTOM_OAUTH_SERVER',
+        )}/v1/authorize*`,
+        (req) => {
+          req.redirect(defaultReturnUrl);
+        },
+      ).as('authRedirect');
+
+      cy.get('[data-cy=sign-in-button]').click();
+      cy.waitFor('@authRedirect').then(() => {
+        cy.url().should('include', defaultReturnUrl);
+      });
+    });
+  });
+
   context('General IDAPI failure', () => {
     it('displays a generic error message', function () {
       cy.mockNext(500);
