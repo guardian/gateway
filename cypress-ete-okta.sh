6c0da77c15c2be93a3d9c8dceba405638792eb4d
Oliver Barnwell <barnwelloliver@gmail.com>
2022-02-09 17:37:08 +0000

    fix: try new glob path for test files
    fix: try ignoring  status code during cypress run

--- cypress-ete-okta.sh
+++ cypress-ete-okta.sh
@@ -23,9 +23,5 @@ else
   yarn build
   yarn start &
   yarn wait-on:server
-<<<<<<< HEAD
-  yarn cypress open --env $CI_ENV --config '{"testFiles":["ete-okta/**/*.ts"]}'
-=======
-  yarn cypress open --env $CI_ENV --config '{"testFiles":["ete_okta/**/*.ts"]}'
->>>>>>> eb5eebe5 (feat: POC of running our existing e2e tests against the new Okta Sign In implementation. this changes the structure so that the test logic lives in its own file: sign_in.shared.ts. Both the Okta and IDAPI sign in e2e tests then import the test logic as methods which are constructed with a flag to switch Okta on if it is truthy.)
+  yarn cypress run --env $CI_ENV --config '{"testFiles":["ete-okta/**/*.ts"]}'
 fi
