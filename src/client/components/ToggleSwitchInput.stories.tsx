07b5412c85a935c4ffcbc599b50a1d45b4283a78
vanessa <32312712+vlbee@users.noreply.github.com>
2022-02-18 13:05:38 +0000

    Add ToggleSwitchInput

---
+++ src/client/components/ToggleSwitchInput.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+
+import { ToggleSwitchInput, ToggleSwitchInputProps } from './ToggleSwitchInput';
+
+export default {
+  title: 'Components/ToggleSwitchInput',
+  component: ToggleSwitchInput,
+  args: {},
+} as Meta<ToggleSwitchInputProps>;
+
+// *****************************************************************************
+
+export const NoLabel = (props: Partial<ToggleSwitchInputProps>) => (
+  <ToggleSwitchInput {...props} />
+);
+NoLabel.storyName = 'Default form switch';
+
+// *****************************************************************************
+
+export const WithLabel = (props: Partial<ToggleSwitchInputProps>) => (
+  <ToggleSwitchInput label={'I am a label'} {...props} />
+);
+
+WithLabel.storyName = 'Form switch with label';
