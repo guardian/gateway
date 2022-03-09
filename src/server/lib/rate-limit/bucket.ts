9f464ac27de1c37df9ea3ff085e358a647a15fe6
Oliver Barnwell <barnwelloliver@gmail.com>
2022-03-09 12:54:48 +0000

    docs: add some documentation around the token bucket algorithm impkl

--- src/server/lib/rate-limit/bucket.ts
+++ src/server/lib/rate-limit/bucket.ts
@@ -8,6 +8,55 @@ import type {
   PipelinedBucketData,
 } from './types';
 
+/**
+ * Implements the token bucket rate limiting algorithm.
+ *
+ * A token bucket to evaluate is passed into the method, along with
+ * the bucket definition which defines the maximum capacity
+ * and the rate at which new tokens are added to the bucket.
+ *
+ * If the bucket or bucket definition are undefined, we cannot
+ * run the algorithm because we do not have enough information
+ * so we exit early.
+ *
+ * There are then two possible paths of execution:
+ *
+ * Bucket does not exist in Redis:
+ * ---
+ *
+ * We skip the token bucket algorithm
+ * and write a new bucket record to Redis with the capacity set
+ * to the maximum capacity minus one.
+ *
+ * Bucket exists in Redis:
+ * ---
+ *
+ * The delta in time that has passed since the bucket was
+ * last updated is used to calculate how many tokens to
+ * add back to the bucket.
+ *
+ * The new bucket token count is then determined by
+ * taking the bucket token count plus the new tokens or
+ * the maximum bucket capacity, whichever is the smaller
+ * of the two.
+ *
+ * If the bucket token count is still zero or less
+ * we write the new bucket state back to Redis and then
+ * return true, indicating that the rate limit has
+ * been hit.
+ *
+ * If the bucket still has tokens remaining, a token is
+ * subtracted from the bucket token count and we write the
+ * new bucket state back to Redis. False is returned,
+ * this indicates that the bucket rate limit has not been hit.
+ *
+ * See also {@link https://en.wikipedia.org/wiki/Token_bucket}
+ *
+ * @param bucket The bucket to rate limit against
+ * @param bucketConfiguration The bucket definition
+ * @param pipelinedWrites The Redis pipeline to batch write commands
+ * @returns Boolean representing whether the rate limit has not been hit.
+ */
 export const rateLimitBucket = (
   bucket: ParsedRateLimitBucket | undefined,
   bucketConfiguration: BucketConfiguration | undefined,
