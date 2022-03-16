615bc1db2d3efd232ec10ae3e7c02c60cb431c9e
Liam Duffy <3338808+liamness@users.noreply.github.com>
2022-03-16 11:04:54 +0000

    Roll out prompt to 100%

--- src/shared/model/experiments/abTests.ts
+++ src/shared/model/experiments/abTests.ts
@@ -1,7 +1,6 @@
 import { AB, ABTest, Participations } from '@guardian/ab-core';
 import { abSwitches } from './abSwitches';
 import { optInPrompt } from './tests/opt-in-prompt';
-import { optInPromptImage } from './tests/opt-in-prompt-image';
 
 interface ABTestConfiguration {
   abTestSwitches: Record<string, boolean>;
@@ -11,7 +10,7 @@ interface ABTestConfiguration {
 }
 
 // Add AB tests to run in this array
-export const tests: ABTest[] = [optInPrompt, optInPromptImage];
+export const tests: ABTest[] = [optInPrompt];
 
 const getDefaultABTestConfiguration = (): ABTestConfiguration => ({
   abTestSwitches: abSwitches,
--- src/shared/model/experiments/tests/opt-in-prompt.ts
+++ src/shared/model/experiments/tests/opt-in-prompt.ts
@@ -9,10 +9,11 @@ export const optInPrompt: ABTest = {
   author: 'liam.duffy.freelancer@guardian.co.uk',
   description:
     'Testing prompting users to opt in to newsletter / marketing after sign in',
-  audience: 0.1,
+  audience: 1,
   audienceOffset: 0,
   successMeasure: 'Understand impact on opt in rates',
-  audienceCriteria: 'Half of users will see prompt after signing in',
+  audienceCriteria:
+    'Half of users viewing consent flow will see prompt after signing in',
   canRun: () => true,
   variants: [
     {
