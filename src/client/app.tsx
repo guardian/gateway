e183a8a58404c8ac2369f225005b07c0d6216b12
Liam Duffy <3338808+liamness@users.noreply.github.com>
2022-03-23 11:10:37 +0000

    Revert usage of resets from source

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { css, Global } from '@emotion/react';
-import { resets } from '@guardian/source-foundations';
+import { neutral } from '@guardian/source-foundations';
 
 import { fontFaces } from '@/client/lib/fonts';
 import { ClientStateProvider } from '@/client/components/ClientState';
@@ -30,18 +30,24 @@ export const App = (props: ClientState) => {
     <>
       <Global
         styles={css`
-          ${resets.defaults}
           ${fontFaces}
           html {
             height: 100%;
+            box-sizing: border-box;
           }
           body {
             height: 100%;
+            color: ${neutral[7]};
           }
           #app {
             min-height: 100%;
             display: flex;
             flex-direction: column;
+          }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
           }
           // Badge is hidden for Gateway, because we're using
           // the legal text to do this job
