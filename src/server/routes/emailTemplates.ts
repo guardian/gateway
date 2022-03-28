d093d46e24b5b883a32a06a06918cde41e23710d
Raphael Kabo <raphaelkabo@hey.com>
2022-03-28 16:13:27 +0100

    fix: Refactor email templating into function
    Co-authored-by: Oliver Barnwell <barnwelloliver@gmail.com>

--- src/server/routes/emailTemplates.ts
+++ src/server/routes/emailTemplates.ts
@@ -9,32 +9,43 @@ import { VerifyText } from '../../email/templates/Verify/VerifyText';
 
 const router = Router();
 
-/* GET a valid email template name. Set the query paramater ?plain=true to
- * receive the email template as plain text. Returns 404 for invalid template
+const emailTemplateTypes = ['ResetPassword', 'Verify'] as const;
+type EMAIL_TEMPLATE_TYPE = typeof emailTemplateTypes[number];
+
+type EMAIL_RENDER_RESULT = {
+  plain: string;
+  html: string;
+};
+
+const renderEmailTemplate = (template: EMAIL_TEMPLATE_TYPE) => {
+  switch (template) {
+    case 'ResetPassword':
+      return {
+        plain: ResetPasswordText(),
+        html: render(ResetPassword()).html,
+      } as EMAIL_RENDER_RESULT;
+    case 'Verify':
+      return {
+        plain: VerifyText(),
+        html: render(Verify()).html,
+      } as EMAIL_RENDER_RESULT;
+    default:
+      // We don't want to do anything for invalid template names
+      return undefined;
+  }
+};
+
+/* GET a valid email template name. Returns 404 for invalid template
  * names. */
 router.get(
   '/email/:template',
   (req: Request, res: ResponseWithRequestState) => {
-    const template = req.params.template;
-    const renderAsPlainText = !!req.query.plain;
-    let plainText, html;
-    switch (template) {
-      case 'ResetPassword':
-        plainText = ResetPasswordText();
-        ({ html } = render(ResetPassword()));
-        break;
-      case 'Verify':
-        plainText = VerifyText();
-        ({ html } = render(Verify()));
-        break;
-      default:
-        // We don't want to do anything for invalid template names
-        return res.sendStatus(404);
-    }
+    const template = req.params.template as EMAIL_TEMPLATE_TYPE;
+    const templateIsValid = emailTemplateTypes.includes(template);
 
-    return renderAsPlainText
-      ? res.send(plainText)
-      : res.type('html').send(html);
+    return templateIsValid
+      ? res.json(renderEmailTemplate(template))
+      : res.send(406); // 406: Not Acceptable
   },
 );
 
