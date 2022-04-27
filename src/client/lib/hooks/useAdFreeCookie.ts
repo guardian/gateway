572534bbb7ece011bf45e51226184751a4aacb43
vanessa <32312712+vlbee@users.noreply.github.com>
2022-04-27 10:28:20 +0100

    Add comments for hooks

--- src/client/lib/hooks/useAdFreeCookie.ts
+++ src/client/lib/hooks/useAdFreeCookie.ts
@@ -3,7 +3,6 @@ import { getCookie } from '@guardian/libs';
 
 // User Attributes API cookies are dropped on subscription on support.theguardian.com for use by frontend
 // User Cookie Information: github.com/guardian/members-data-api/blob/main/README.md
-
 export const useAdFreeCookie: () => boolean = () => {
   const [hasAdFree, setHasAdFree] = useState(false);
   const [isDigitalSubscriber, setIsDigitalSubscriber] = useState(false);
--- src/client/lib/hooks/useCmpConsent.ts
+++ src/client/lib/hooks/useCmpConsent.ts
@@ -2,6 +2,11 @@ import { useState, useEffect } from 'react';
 import { ConsentState } from '@guardian/consent-management-platform/dist/types';
 import { onConsentChange } from '@guardian/consent-management-platform';
 
+/**
+ * This hook checks the CMP (Consent Management Platform) browser cookies. It returns true only if everything
+ * has been consented to. This code is lifted from the Sign In Gate Display Rules in DotCom-Rendering repo:
+ * https://github.com/guardian/dotcom-rendering/blob/main/dotcom-rendering/src/web/components/SignInGate/displayRule.ts#L71-L105
+ */
 const hasRequiredConsents = (): Promise<boolean> => {
   const hasConsentedToAll = (state: ConsentState) => {
     const consentFlags = state.tcfv2?.consents
