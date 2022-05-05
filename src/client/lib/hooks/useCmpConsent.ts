c620c5ba045edbe9f5e023f2246cd5074bdfd92f
vanessa <32312712+vlbee@users.noreply.github.com>
2022-05-05 16:06:50 +0100

    Refactor useCmpConsent

--- src/client/lib/hooks/useCmpConsent.ts
+++ src/client/lib/hooks/useCmpConsent.ts
@@ -1,25 +1,17 @@
 import { useState, useEffect } from 'react';
 import { ConsentState } from '@guardian/consent-management-platform/dist/types';
-import { onConsentChange } from '@guardian/consent-management-platform';
+import { onConsent } from '@guardian/consent-management-platform';
 
 /**
- * ConsentState.canTarget checks if user has consented to all consents
- * https://github.com/guardian/consent-management-platform#consentstatecantarget
+ * More info on CMP:
+ * https://github.com/guardian/consent-management-platform
  */
-const hasRequiredCmpConsents = (): Promise<boolean> => {
-  return new Promise((resolve) => {
-    onConsentChange((state: ConsentState) => {
-      return resolve(state.canTarget);
-    });
-  });
-};
-
-// Returns true or false if a user has consented to all CMP consents for their region
 export const useCmpConsent: () => boolean = () => {
   const [hasCmpConsent, setCmpConsent] = useState(false);
   useEffect(() => {
     const checkCmpConsent = async () => {
-      setCmpConsent(await hasRequiredCmpConsents());
+      const consentState: ConsentState = await onConsent();
+      setCmpConsent(consentState.canTarget); // canTarget if user has consented to all consents
     };
     checkCmpConsent();
   }, []);
