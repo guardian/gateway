31b0526add34cf11a8df1d53b9f7175288f81499
Oliver Barnwell <barnwelloliver@gmail.com>
2022-05-06 17:24:09 +0100

    feat: finish useInputValidityState hook

--- src/client/components/EmailInput.tsx
+++ src/client/components/EmailInput.tsx
@@ -1,27 +1,32 @@
 import { TextInput, TextInputProps } from '@guardian/source-react-components';
-import React, { useState } from 'react';
+import React from 'react';
 import {
   disableAutofillBackground,
   noBorderRadius,
 } from '@/client/styles/Shared';
+import {
+  InputFieldState,
+  useInputValidityState,
+} from '@/client/lib/hooks/useInputValidityState';
 
-export enum InputFieldState {
-  VALID = 'valid',
-  EMPTY = 'empty',
-  INVALID = 'invalid',
+interface EmailInputProps extends Omit<TextInputProps, 'label'> {
+  label?: string;
+  defaultValue?: string;
+  initialState?: InputFieldState;
 }
 
-enum InputFieldErrorMessages {
-  EMAIL_INVALID = 'Please enter a valid email format.',
-  EMAIL_EMPTY = 'Please enter your email.',
-
-  FIRST_NAME_EMPTY = 'Please enter your First name',
-  LAST_NAME_EMPTY = 'Please enter your Last name',
+enum EmailInputFieldErrorMessages {
+  INVALID = 'Please enter a valid email format.',
+  EMPTY = 'Please enter your email.',
 }
 
-const useInputValidityState = (initialState: InputFieldState) => {
-  const [inputFieldState, setInputFieldState] =
-    useState<InputFieldState>(initialState);
+export const EmailInput: React.FC<EmailInputProps> = ({
+  label = 'Email',
+  initialState = InputFieldState.VALID,
+  ...rest
+}) => {
+  const { onBlur, onInput, onInvalid, inputFieldState } =
+    useInputValidityState(initialState);
 
   // Set the error message based on the current state.
   const errorMessage = React.useMemo(() => {
@@ -33,49 +38,6 @@ const useInputValidityState = (initialState: InputFieldState) => {
     }
   }, [inputFieldState]);
 
-  return {
-    onBlur: undefined,
-    onInput: undefined,
-    onInvalid: undefined,
-  };
-};
-
-interface EmailInputProps extends Omit<TextInputProps, 'label'> {
-  label?: string;
-  defaultValue?: string;
-  initialState?: InputFieldState;
-}
-
-export const EmailInput: React.FC<EmailInputProps> = ({
-  label = 'Email',
-  initialState = EmailInputFieldState.VALID,
-  ...rest
-}) => {
-  // Transition error message state based upon the
-  // HTML ValidityState object returned from the input.
-  const transitionState = (validityState: ValidityState) => {
-    const emailInputIsInvalid =
-      validityState.customError ||
-      validityState.badInput ||
-      validityState.patternMismatch ||
-      validityState.rangeOverflow ||
-      validityState.rangeUnderflow ||
-      validityState.tooLong ||
-      validityState.tooShort ||
-      validityState.stepMismatch ||
-      validityState.typeMismatch;
-
-    const emailInputIsEmpty = validityState.valueMissing;
-
-    if (emailInputIsEmpty) {
-      setEmailInputFieldState(EmailInputFieldState.EMPTY);
-    } else if (emailInputIsInvalid) {
-      setEmailInputFieldState(EmailInputFieldState.INVALID);
-    } else {
-      setEmailInputFieldState(EmailInputFieldState.VALID);
-    }
-  };
-
   return (
     <TextInput
       label={label}
@@ -84,28 +46,9 @@ export const EmailInput: React.FC<EmailInputProps> = ({
       autoComplete="email"
       error={errorMessage}
       cssOverrides={[disableAutofillBackground, noBorderRadius]}
-      onBlur={(e) => {
-        // Transition error state when the input box loses focus.
-        transitionState(e.target.validity);
-      }}
-      onInput={(e) => {
-        // We check the `composed` variable to see if the event originated from user input.
-        // This is so that we can run validation when users submit an email through a password manager.
-        // Composed is not supported in IE11, so we check to see if it's undefined first.
-        const notOriginatingFromUserInput =
-          e.nativeEvent.composed !== undefined && !e.nativeEvent.composed;
-
-        if (notOriginatingFromUserInput) {
-          e.currentTarget.checkValidity();
-          transitionState(e.currentTarget.validity);
-        }
-      }}
-      onInvalid={(e) => {
-        // Prevent default browser popup from firing.
-        e.preventDefault();
-        // Transition error state when the invalid event fires.
-        transitionState(e.currentTarget.validity);
-      }}
+      onBlur={onBlur}
+      onInput={onInput}
+      onInvalid={onInvalid}
       {...rest}
     />
   );
---
+++ src/client/lib/hooks/useInputValidityState.ts
@@ -0,0 +1,87 @@
+import { useCallback, useState } from 'react';
+
+export enum InputFieldState {
+  VALID = 'valid',
+  EMPTY = 'empty',
+  INVALID = 'invalid',
+}
+
+export const useInputValidityState = (
+  initialState: InputFieldState,
+): {
+  onBlur: React.FocusEventHandler<HTMLInputElement>;
+  onInput: React.FormEventHandler<HTMLInputElement>;
+  onInvalid: React.FormEventHandler<HTMLInputElement>;
+  transitionState: (validityState: ValidityState) => void;
+  inputFieldState: InputFieldState;
+} => {
+  const [inputFieldState, setInputFieldState] =
+    useState<InputFieldState>(initialState);
+
+  // Transition error message state based upon the
+  // HTML ValidityState object returned from the input.
+  const transitionState = useCallback((validityState: ValidityState) => {
+    const emailInputIsInvalid =
+      validityState.customError ||
+      validityState.badInput ||
+      validityState.patternMismatch ||
+      validityState.rangeOverflow ||
+      validityState.rangeUnderflow ||
+      validityState.tooLong ||
+      validityState.tooShort ||
+      validityState.stepMismatch ||
+      validityState.typeMismatch;
+
+    const emailInputIsEmpty = validityState.valueMissing;
+
+    if (emailInputIsEmpty) {
+      setInputFieldState(InputFieldState.EMPTY);
+    } else if (emailInputIsInvalid) {
+      setInputFieldState(InputFieldState.INVALID);
+    } else {
+      setInputFieldState(InputFieldState.VALID);
+    }
+  }, []);
+
+  const onBlur = useCallback<React.FocusEventHandler<HTMLInputElement>>(
+    (e) => {
+      // Transition error state when the input box loses focus.
+      transitionState(e.target.validity);
+    },
+    [transitionState],
+  );
+
+  const onInput = useCallback<React.FormEventHandler<HTMLInputElement>>(
+    (e) => {
+      // We check the `composed` variable to see if the event originated from user input.
+      // This is so that we can run validation when users submit an email through a password manager.
+      // Composed is not supported in IE11, so we check to see if it's undefined first.
+      const notOriginatingFromUserInput =
+        e.nativeEvent.composed !== undefined && !e.nativeEvent.composed;
+
+      if (notOriginatingFromUserInput) {
+        e.currentTarget.checkValidity();
+        transitionState(e.currentTarget.validity);
+      }
+    },
+    [transitionState],
+  );
+
+  const onInvalid = useCallback<React.FormEventHandler<HTMLInputElement>>(
+    (e) => {
+      // Prevent default browser popup from firing.
+      e.preventDefault();
+      // Transition error state when the invalid event fires.
+      transitionState(e.currentTarget.validity);
+    },
+    [transitionState],
+  );
+
+  return {
+    onBlur,
+    onInput,
+    onInvalid,
+    transitionState,
+    inputFieldState,
+  };
+};
