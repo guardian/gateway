99a6549a5b15eb216c130b2e3cae2657a07d0e79
Raphael Kabo <raphaelkabo@hey.com>
2022-05-12 17:20:41 +0100

    feat: Add ETE and Jest tests for Okta registration

--- cypress/support/commands/testUser.ts
+++ cypress/support/commands/testUser.ts
@@ -1,10 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
+import { TokenResponse, UserResponse } from '@/server/models/okta/User';
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       createTestUser: typeof createTestUser;
+      getTestOktaUser: typeof getTestOktaUser;
+      activateTestOktaUser: typeof activateTestOktaUser;
+      resetOktaUserPassword: typeof resetOktaUserPassword;
+      expireOktaUserPassword: typeof expireOktaUserPassword;
+      suspendOktaUser: typeof suspendOktaUser;
     }
   }
 }
@@ -88,5 +94,114 @@ export const createTestUser = ({
       });
   } catch (error) {
     throw new Error('Failed to create IDAPI test user: ' + error);
+  }
+};
+
+export const getTestOktaUser = (id: string) => {
+  try {
+    return cy
+      .request({
+        url: `${Cypress.env('OKTA_ORG_URL')}/api/v1/users/${id}`,
+        method: 'GET',
+        headers: {
+          Authorization: `SSWS ${Cypress.env('OKTA_API_TOKEN')}`,
+        },
+        retryOnStatusCodeFailure: true,
+      })
+      .then((res) => {
+        const user = res.body as UserResponse;
+        return cy.wrap(user);
+      });
+  } catch (error) {
+    throw new Error('Failed to create Okta test user: ' + error);
+  }
+};
+export const activateTestOktaUser = (id: string) => {
+  try {
+    return cy
+      .request({
+        url: `${Cypress.env(
+          'OKTA_ORG_URL',
+        )}/api/v1/users/${id}/lifecycle/activate`,
+        method: 'POST',
+        headers: {
+          Authorization: `SSWS ${Cypress.env('OKTA_API_TOKEN')}`,
+        },
+        qs: {
+          sendEmail: false,
+        },
+        retryOnStatusCodeFailure: true,
+      })
+      .then((res) => {
+        const token = res.body as TokenResponse;
+        return cy.wrap(token);
+      });
+  } catch (error) {
+    throw new Error('Failed to activate Okta test user: ' + error);
+  }
+};
+export const resetOktaUserPassword = (id: string) => {
+  try {
+    return cy
+      .request({
+        url: `${Cypress.env(
+          'OKTA_ORG_URL',
+        )}/api/v1/users/${id}/lifecycle/reset_password`,
+        method: 'POST',
+        headers: {
+          Authorization: `SSWS ${Cypress.env('OKTA_API_TOKEN')}`,
+        },
+        qs: {
+          sendEmail: false,
+        },
+        retryOnStatusCodeFailure: true,
+      })
+      .then((res) => {
+        const token = res.body.resetPasswordUrl.split('/').slice(-1)[0];
+        return cy.wrap({ token } as TokenResponse);
+      });
+  } catch (error) {
+    throw new Error('Failed to reset password for Okta test user: ' + error);
+  }
+};
+export const expireOktaUserPassword = (id: string) => {
+  try {
+    return cy
+      .request({
+        url: `${Cypress.env(
+          'OKTA_ORG_URL',
+        )}/api/v1/users/${id}/lifecycle/expire_password`,
+        method: 'POST',
+        headers: {
+          Authorization: `SSWS ${Cypress.env('OKTA_API_TOKEN')}`,
+        },
+        retryOnStatusCodeFailure: true,
+      })
+      .then((res) => {
+        const user = res.body as UserResponse;
+        return cy.wrap(user);
+      });
+  } catch (error) {
+    throw new Error('Failed to expire password for Okta test user: ' + error);
+  }
+};
+export const suspendOktaUser = (id: string) => {
+  try {
+    return cy
+      .request({
+        url: `${Cypress.env(
+          'OKTA_ORG_URL',
+        )}/api/v1/users/${id}/lifecycle/suspend`,
+        method: 'POST',
+        headers: {
+          Authorization: `SSWS ${Cypress.env('OKTA_API_TOKEN')}`,
+        },
+        retryOnStatusCodeFailure: true,
+      })
+      .then(() => {
+        return cy.wrap(true);
+      });
+  } catch (error) {
+    throw new Error('Failed to suspend Okta test user: ' + error);
   }
 };
