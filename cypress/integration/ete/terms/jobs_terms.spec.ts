a6a06d40ba2170839849c600109ad01a6742cb91
Oliver Barnwell <barnwelloliver@gmail.com>
2022-05-13 17:40:59 +0100

    feat: finish test suite for cases where session is not set/invalid. stub case for when they have no first/last name

--- cypress/integration/ete/terms/jobs_terms.spec.ts
+++ cypress/integration/ete/terms/jobs_terms.spec.ts
@@ -1,6 +1,32 @@
 describe('Jobs terms and conditions flow', () => {
   context('Accepts Jobs terms and conditions and sets their name', () => {
-    it.only('should not show the page for a user who has already accepted the terms', () => {
+    it('should redirect users with an invalid SC_GU_U cookie to the signin page', () => {
+      // load the consents page as its on the same domain
+      const termsAcceptPageUrl = `https://${Cypress.env(
+        'BASE_URI',
+      )}/agree/GRS?returnUrl=https://profile.thegulocal.com/signin?returnUrl=https%3A%2F%2Fm.code.dev-theguardian.com%2F`;
+
+      cy.setCookie('SC_GU_U', 'invalid-cookie');
+
+      cy.visit(termsAcceptPageUrl);
+      cy.url().should('include', 'https://profile.thegulocal.com/signin');
+    });
+
+    it('should redirect users with no SC_GU_U cookie to the signin page', () => {
+      // load the consents page as its on the same domain
+      const termsAcceptPageUrl = `https://${Cypress.env(
+        'BASE_URI',
+      )}/agree/GRS?returnUrl=https://profile.thegulocal.com/healthcheck`;
+      cy.visit(termsAcceptPageUrl);
+      cy.url().should(
+        'include',
+        'https://profile.thegulocal.com/signin?returnUrl=https%3A%2F%2Fprofile.thegulocal.com%2Fhealthcheck',
+      );
+    });
+
+    // it('should show the jobs terms page for users who do not have first/last name set, but are part of GRS', () => {});
+
+    it('should redirect users who have already accepted the terms away', () => {
       cy.createTestUser({
         isUserEmailValidated: true,
       })?.then(({ emailAddress, finalPassword }) => {
@@ -28,32 +54,30 @@ describe('Jobs terms and conditions flow', () => {
 
         cy.contains('Welcome to Guardian Jobs');
 
-        cy.get('input[name=firstName]').clear().type('First Name');
-        cy.get('input[name=secondName]').clear().type('Second Name');
-
-        // User should not be in GRS and not have our intentional first/last name.
+        // User should not be in GRS and have the original first/last name.
         cy.getTestUserDetails().then(({ user, status }) => {
           expect(status).to.eq('ok');
-          expect(user.privateFields.firstName).not.to.eq('First Name');
-          expect(user.privateFields.secondName).not.to.eq('Second Name');
+          cy.get('input[name=firstName]').should(
+            'contain.value',
+            user.privateFields.firstName,
+          );
+          cy.get('input[name=secondName]').should(
+            'contain.value',
+            user.privateFields.secondName,
+          );
+
           const joinedGroups = user.userGroups.map(
             (group) => group.packageCode,
           );
           expect(joinedGroups).not.to.include('GRS');
         });
 
-        // Intercept the external redirect page.
-        // We just want to check that the redirect happens, not that the page loads.
-        cy.intercept('GET', 'https://jobs.theguardian.com/', (req) => {
-          req.reply(200);
-        });
+        cy.get('input[name=firstName]').clear().type('First Name');
+        cy.get('input[name=secondName]').clear().type('Second Name');
 
         cy.findByText('Continue').click();
 
-        // Make sure the returnURL is respected.
-        // cy.url().should('include', 'https://jobs.theguardian.com/');
-
-        // User should be in the GRS group and have First/Last name set.
+        // User should be in the GRS group and have First/Last name set to our custom values.
         cy.getTestUserDetails().then(({ user, status }) => {
           expect(status).to.eq('ok');
           expect(user.privateFields.firstName).to.eq('First Name');
@@ -64,10 +88,22 @@ describe('Jobs terms and conditions flow', () => {
           expect(joinedGroups).to.include('GRS');
         });
 
+        cy.url().should(
+          'include',
+          'https://profile.thegulocal.com/healthcheck',
+        );
+
         const finalTermsAcceptPageUrl = `https://${Cypress.env(
           'BASE_URI',
-        )}/agree/GRS?returnUrl=https://profile.thegulocal.com/404`;
-        cy.visit(finalTermsAcceptPageUrl);
+        )}/agree/GRS?returnUrl=https://profile.thegulocal.com/maintenance`;
+
+        cy.visit(finalTermsAcceptPageUrl, { failOnStatusCode: false });
+
+        // Make sure the returnURL is respected.
+        cy.url().should(
+          'include',
+          'https://profile.thegulocal.com/maintenance',
+        );
       });
     });
 
@@ -99,19 +135,26 @@ describe('Jobs terms and conditions flow', () => {
 
         cy.contains('Welcome to Guardian Jobs');
 
-        cy.get('input[name=firstName]').clear().type('First Name');
-        cy.get('input[name=secondName]').clear().type('Second Name');
-
-        // User should not be in GRS and not have our intentional first/last name.
+        // User should not be in GRS and have the original first/last name.
         cy.getTestUserDetails().then(({ user, status }) => {
           expect(status).to.eq('ok');
-          expect(user.privateFields.firstName).not.to.eq('First Name');
-          expect(user.privateFields.secondName).not.to.eq('Second Name');
+          cy.get('input[name=firstName]').should(
+            'contain.value',
+            user.privateFields.firstName,
+          );
+          cy.get('input[name=secondName]').should(
+            'contain.value',
+            user.privateFields.secondName,
+          );
+
           const joinedGroups = user.userGroups.map(
             (group) => group.packageCode,
           );
           expect(joinedGroups).not.to.include('GRS');
         });
+
+        cy.get('input[name=firstName]').clear().type('First Name');
+        cy.get('input[name=secondName]').clear().type('Second Name');
 
         // Intercept the external redirect page.
         // We just want to check that the redirect happens, not that the page loads.
