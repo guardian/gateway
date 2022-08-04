6c57861640dd6bc53a6904394a0db311b80c6bff
Oliver Barnwell <barnwelloliver@gmail.com>
2022-08-04 18:29:48 +0100

    feat: write out to file in the script, and add a helpful message

--- scripts/generate-rate-limit-schema.ts
+++ scripts/generate-rate-limit-schema.ts
@@ -1,11 +1,18 @@
+import { writeFileSync } from 'node:fs';
 import zodToJsonSchema from 'zod-to-json-schema';
 import { rateLimiterConfigurationSchema } from '../src/server/lib/rate-limit/configurationValidator';
 
+process.stdout.write('✨  Generating rate limit configuration schema  ✨\n');
+
+// Generate a JSON schema for the rate limiter configuration.
 const jsonSchema = zodToJsonSchema(
   rateLimiterConfigurationSchema,
   'rateLimiterConfigurationSchema',
 );
 
-// Output the JSON schema for the rate limiter configuration.
-// We use this for autocompletion when editing the rate limit configuration file.
-process.stdout.write(JSON.stringify(jsonSchema) + '\n');
+// Write the pretty printed JSON schema to .ratelimit.schema.json.
+// We use this for autocompletion when editing the rate limit configuration file (and example).
+writeFileSync(
+  '.ratelimit.schema.json',
+  JSON.stringify(jsonSchema, null, 2) + '\n',
+);
