434e040a25d09a1c1e9236d60e975d32fc8e9c56
Patricio Vighi <pvighi@gmail.com>
2022-09-07 17:33:45 +0100

    remove test duplication
    because of the mocking a few tests ended up effectively  being the same but running 5 times each

--- cypress/integration/mocked-okta/signInController.2.cy.ts
+++ cypress/integration/mocked-okta/signInController.2.cy.ts
@@ -6,102 +6,81 @@ import validUserGroupsResponse from '../../fixtures/okta-responses/success/valid
 beforeEach(() => {
   cy.mockPurge();
 });
-
-userStatuses.forEach((state) => {
-  context(`Given I am a ${state || 'nonexistent'} user`, () => {
-    context('When I submit the form on /signin', () => {
-      beforeEach(() => {
-        cy.visit(`/signin?useOkta=true`);
-        cy.get('input[name="email"]').type('example@example.com');
-        cy.get('input[name="password"]').type('password');
-      });
-      switch (state) {
-        case false:
-        case 'PROVISIONED':
-        case 'STAGED':
-        case 'RECOVERY':
-        case 'PASSWORD_EXPIRED':
-          specify(
-            'Then I should be shown an "Email or password incorrect" error.',
-            () => {
-              cy.mockNext(
-                authenticationFailedError.code,
-                authenticationFailedError.response,
-              );
-              cy.get('button[type=submit]').click();
-              cy.contains("Email and password don't match");
-            },
-          );
-          break;
-        // When I am an ACTIVE user
-        case 'ACTIVE':
-          specify('Then I should be signed in.', () => {
-            cy.mockNext(validUserResponse.code, validUserResponse.response);
-            cy.mockNext(
-              validUserGroupsResponse.code,
-              validUserGroupsResponse.response,
-            );
-
-            cy.get('button[type=submit]').click();
-            // we can't actually check the authorization code flow
-            // so simply intercept the request and verify that it contains
-            // the mocked response
-            cy.intercept(
-              `http://localhost:9000/oauth2/${Cypress.env(
-                'OKTA_CUSTOM_OAUTH_SERVER',
-              )}/v1/authorize*`,
-              'OAuth',
-            );
-            cy.contains('OAuth');
-          });
-      }
-    });
-    context('When I submit the form on /reauthenticate', () => {
-      beforeEach(() => {
-        cy.visit(`/reauthenticate?useOkta=true`);
-        cy.get('input[name="email"]').type('example@example.com');
-        cy.get('input[name="password"]').type('password');
-      });
-      switch (state) {
-        case false:
-        case 'PROVISIONED':
-        case 'STAGED':
-        case 'RECOVERY':
-        case 'PASSWORD_EXPIRED':
-          specify(
-            'Then I should be shown an "Email or password incorrect" error.',
-            () => {
-              cy.mockNext(
-                authenticationFailedError.code,
-                authenticationFailedError.response,
-              );
-              cy.get('button[type=submit]').click();
-              cy.contains("Email and password don't match");
-            },
-          );
-          break;
-        // When I am an ACTIVE user
-        case 'ACTIVE':
-          specify('Then I should be signed in.', () => {
-            cy.mockNext(validUserResponse.code, validUserResponse.response);
-            cy.mockNext(
-              validUserGroupsResponse.code,
-              validUserGroupsResponse.response,
-            );
-
-            cy.get('button[type=submit]').click();
-            // we can't actually check the authorization code flow
-            // so simply intercept the request and verify that it contains
-            // the mocked response
-            cy.intercept(
-              `http://localhost:9000/oauth2/${Cypress.env(
-                'OKTA_CUSTOM_OAUTH_SERVER',
-              )}/v1/authorize*`,
-              'OAuth',
-            );
-            cy.contains('OAuth');
-          });
-      }
-    });
+context('When I submit the form on /signin', () => {
+  beforeEach(() => {
+    cy.visit(`/signin?useOkta=true`);
+    cy.get('input[name="email"]').type('example@example.com');
+    cy.get('input[name="password"]').type('password');
   });
+  //This is expected for non existent, non active users or when the email and password are incorrect
+  specify(
+    'if okta authentication fails then I should be shown an "Email or password incorrect" error. ',
+    () => {
+      cy.mockNext(
+        authenticationFailedError.code,
+        authenticationFailedError.response,
+      );
+      cy.get('button[type=submit]').click();
+      cy.contains("Email and password don't match");
+    },
+  );
+  specify('if okta authentication succeeds then I should be signed in.', () => {
+    cy.mockNext(validUserResponse.code, validUserResponse.response);
+    cy.mockNext(validUserGroupsResponse.code, validUserGroupsResponse.response);
+
+    cy.get('button[type=submit]').click();
+    // we can't actually check the authorization code flow
+    // so simply intercept the request and verify that it contains
+    // the mocked response
+    cy.intercept(
+      `http://localhost:9000/oauth2/${Cypress.env(
+        'OKTA_CUSTOM_OAUTH_SERVER',
+      )}/v1/authorize*`,
+      'OAuth',
+    );
+    cy.contains('OAuth');
+  });
+});
+
+context('When I submit the form on /reauthenticate', () => {
+  beforeEach(() => {
+    cy.visit(`/reauthenticate?useOkta=true`);
+    cy.get('input[name="email"]').type('example@example.com');
+    cy.get('input[name="password"]').type('password');
+  });
+  //This is expected for non existent, non active users or when the email and password are incorrect
+  specify(
+    'If okta authentication fails then I should be shown an "Email or password incorrect" error.',
+    () => {
+      cy.mockNext(
+        authenticationFailedError.code,
+        authenticationFailedError.response,
+      );
+      cy.get('button[type=submit]').click();
+      cy.contains("Email and password don't match");
+    },
+  );
+
+  specify(
+    'If okta authentication succeeeds then I should be signed in.',
+    () => {
+      cy.mockNext(validUserResponse.code, validUserResponse.response);
+      cy.mockNext(
+        validUserGroupsResponse.code,
+        validUserGroupsResponse.response,
+      );
+
+      cy.get('button[type=submit]').click();
+      // we can't actually check the authorization code flow
+      // so simply intercept the request and verify that it contains
+      // the mocked response
+      cy.intercept(
+        `http://localhost:9000/oauth2/${Cypress.env(
+          'OKTA_CUSTOM_OAUTH_SERVER',
+        )}/v1/authorize*`,
+        'OAuth',
+      );
+      cy.contains('OAuth');
+    },
+  );
 });
