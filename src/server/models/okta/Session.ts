769ee8c1c9baf71de369e46ad2e2e11133b6fabb
Mahesh Makani <mahesh@makani.dev>
2022-11-02 11:21:44 +0000

    refactor: update Okta `Session` model to use `zod` for schema validation

--- src/server/models/okta/Session.ts
+++ src/server/models/okta/Session.ts
@@ -1,14 +1,15 @@
+import { z } from 'zod';
 // Session object definition https://developer.okta.com/docs/reference/api/sessions/#session-properties
-
-export interface SessionResponse {
-  id: string;
-  login: string;
-  userId: string;
-  expiresAt: string;
-  status: 'ACTIVE' | 'MFA_REQUIRED' | 'MFA_ENROLL';
-  lastPasswordVerification: string;
-  lastFactorVerification: string;
-  // amr: string;
-  // idp: string;
-  mfaActive: boolean;
-}
+export const sessionSchema = z.object({
+  id: z.string(),
+  login: z.string(),
+  userId: z.string(),
+  expiresAt: z.string(),
+  status: z.enum(['ACTIVE', 'MFA_REQUIRED', 'MFA_ENROLL']),
+  lastPasswordVerification: z.string().nullable().optional(),
+  lastFactorVerification: z.string().nullable().optional(),
+  // amr: z.string(),
+  // idp: z.string(),
+  mfaActive: z.boolean(),
+});
+export type SessionResponse = z.infer<typeof sessionSchema>;
