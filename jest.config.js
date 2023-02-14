2ea410b7b999c4ddd5d48c8a58c727f495ad66b6
Mahesh Makani <mahesh@makani.dev>
2023-02-14 09:19:51 +0000

    feat: switch to using preact/compat instead of react

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,12 @@ module.exports = {
     '@/([^\\.]*)$': '<rootDir>/src/$1',
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
+    // need to alias preact to itself in order to use the cjs version, same with testing-library
+    '^preact(/(.*)|$)': 'preact$1',
+    '^@testing-library/react$': '@testing-library/preact',
+    '^@testing-library/react-hooks$': '@testing-library/preact-hooks',
+    '^@testing-library/preact$': '@testing-library/preact',
+    '^@testing-library/preact-hooks$': '@testing-library/preact-hooks',
   },
   testPathIgnorePatterns: [
     '<rootDir>/cypress/',
