aa29d5f6ce0e6afe0779143e4c481d031c7b838f
Mahesh Makani <mahesh@makani.dev>
2023-03-03 10:00:49 +0000

    feat: migrate to aws sdk v3

--- src/server/lib/serverSideLogger.ts
+++ src/server/lib/serverSideLogger.ts
@@ -1,5 +1,6 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { AWSError, Kinesis } from 'aws-sdk';
+import { Kinesis } from '@aws-sdk/client-kinesis';
 import { LogLevel } from '@/shared/model/Logger';
 import { createLogger, transports } from 'winston';
 import Transport, { TransportStreamOptions } from 'winston-transport';
@@ -13,6 +14,8 @@ const {
   aws: { instanceId, kinesisStreamName },
 } = getConfiguration();
 
+const encoder = new TextEncoder();
+
 // custom "Winston Transport" to send logs to the AWS kinesis stream
 // see https://github.com/winstonjs/winston-transport#usage
 class KinesisTransport extends Transport {
@@ -24,7 +27,6 @@ class KinesisTransport extends Transport {
     this.kinesis = new Kinesis(awsConfig);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public log(info: any, callback: () => void) {
     const { level, message, ...extraFields } = info;
 
@@ -37,33 +39,53 @@ class KinesisTransport extends Transport {
       }
 
       if (kinesisStreamName) {
+        const data = JSON.stringify({
+          type: 'app',
+          app: 'identity-gateway',
+          stack: 'identity',
+          path: '/var/log/identity-gateway.log',
+          instance_id: instanceId,
+          stage,
+          level,
+          message,
+          ...extraFields,
+        });
+
         this.kinesis
           .putRecord({
             StreamName: kinesisStreamName,
             PartitionKey: stage,
-            Data: JSON.stringify({
-              type: 'app',
-              app: 'identity-gateway',
-              stack: 'identity',
-              path: '/var/log/identity-gateway.log',
-              instance_id: instanceId,
-              stage,
-              level,
-              message,
-              ...extraFields,
-            }),
+            Data: encoder.encode(data),
           })
-          .promise()
-          .catch((error: AWSError) => {
-            if (error.code.includes('ExpiredToken') && stage === 'DEV') {
-              // eslint-disable-next-line no-console
+          .catch((error: unknown) => {
+            if (error instanceof Error) {
+              if (error.name === 'ExpiredTokenException' && stage === 'DEV') {
+                console.log(error.message);
+                console.warn(
+                  'AWS Credentials Expired. Have you added `Identity` Janus credentials?',
+                );
+              }
+
+              if (error.name === 'TimeoutError') {
+                console.warn(error.message);
+                console.warn(
+                  'Timeout when attempting to log to kinesis stream.',
+                );
+              }
+
+              if (
+                error.name !== 'TimeoutError' &&
+                error.name !== 'ExpiredTokenException'
+              ) {
+                console.warn('Error when attempting to log to kinesis stream.');
+                console.warn(error.name);
+                console.warn(error.message);
+              }
+            } else {
               console.warn(
-                'AWS Credentials Expired. Have you added `Identity` Janus credentials?',
+                'Unknown error when attempting to log to kinesis stream.',
               );
-            }
-            if (error.code.includes('TimeoutError')) {
-              // eslint-disable-next-line no-console
-              console.warn('Timeout when attempting to log to kinesis stream.');
+              console.warn(error);
             }
           });
       }
@@ -84,7 +106,6 @@ const loggingOptions: InspectOptions = {
   compact: true,
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const formatLogParam = (message?: any) =>
   formatWithOptions(loggingOptions, message);
 
