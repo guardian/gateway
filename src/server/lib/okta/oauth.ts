a03495b11f46db49d54fe88957d70c4b9b372d39
Mahesh Makani <mahesh@makani.dev>
2023-05-05 16:47:00 +0100

    refactor: make `performAuthorizationCodeFlow` method more flexible

--- src/server/lib/okta/oauth.ts
+++ src/server/lib/okta/oauth.ts
@@ -5,10 +5,35 @@ import { RoutePaths } from '@/shared/model/Routes';
 import {
   generateAuthorizationState,
   setAuthorizationStateCookie,
-  ProfileOpenIdClientRedirectUris,
   getOpenIdClient,
 } from '@/server/lib/okta/openid-connect';
 import { closeSession } from './api/sessions';
+
+type Scopes =
+  | 'openid'
+  // the `guardian.identity-api.cookies.create.self.secure scope` is checked on IDAPI to return legacy identity cookies on authentication
+  | 'guardian.identity-api.cookies.create.self.secure';
+
+/**
+ * @param closeExistingSession (optional) - if true, we'll close any existing okta session before calling the authorization code flow
+ * @param confirmationPagePath (optional) - page to redirect the user to after authentication
+ * @param doNotSetLastAccessCookie (optional) - if true, does not update the SC_GU_LA cookie during update of Idapi cookies.  Default false.
+ * @param idp (optional) - okta id of the social identity provider to use
+ * @param prompt (optional) - if provided, we'll use this to set the prompt parameter, see https://developer.okta.com/docs/reference/api/oidc/#parameter-details for prompt parameter details, N.B `undefined` has a different meaning to `none`
+ * @param redirectUri - the redirect uri to use for the /authorize endpoint
+ * @param scopes (optional) - any scopes to use for the /authorize endpoint, defaults to ['openid']
+ * @param sessionToken (optional) - if provided, we'll use this to set the session cookie
+ */
+interface PerformAuthorizationCodeFlowOptions {
+  closeExistingSession?: boolean;
+  confirmationPagePath?: RoutePaths;
+  doNotSetLastAccessCookie?: boolean;
+  idp?: string;
+  prompt?: 'login' | 'none';
+  redirectUri: string;
+  scopes?: Scopes[];
+  sessionToken?: string | null;
+}
 
 /**
  * @name performAuthorizationCodeFlow
@@ -18,11 +43,7 @@ import { closeSession } from './api/sessions';
  *
  * @param req - the express request object
  * @param res - the express response object
- * @param sessionToken (optional) - if provided, we'll use this to set the session cookie
- * @param confirmationPagePath (optional) - page to redirect the user to after authentication
- * @param idp (optional) - okta id of the social identity provider to use
- * @param closeExistingSession (optional) - if true, we'll close any existing okta session before calling the authorization code flow
- * @param doNotSetLastAccessCookie (optional) - if true, does not update the SC_GU_LA cookie during update of Idapi cookies.  Default false.
+ * @param options - the options for the authorization code flow
  * @returns 303 redirect to the okta /authorize endpoint
  */
 export const performAuthorizationCodeFlow = async (
@@ -34,13 +55,10 @@ export const performAuthorizationCodeFlow = async (
     idp,
     closeExistingSession,
     doNotSetLastAccessCookie = false,
-  }: {
-    sessionToken?: string | null;
-    confirmationPagePath?: RoutePaths;
-    idp?: string;
-    closeExistingSession?: boolean;
-    doNotSetLastAccessCookie?: boolean;
-  } = {},
+    prompt,
+    scopes = ['openid'],
+    redirectUri,
+  }: PerformAuthorizationCodeFlowOptions,
 ) => {
   if (closeExistingSession) {
     const oktaSessionCookieId: string | undefined = req.cookies.sid;
@@ -66,20 +84,18 @@ export const performAuthorizationCodeFlow = async (
 
   // generate the /authorize endpoint url which we'll redirect the user too
   const authorizeUrl = OpenIdClient.authorizationUrl({
-    // Prompt for login if the idp is provided to make sure the user sees
+    // Prompt for 'login' if the idp is provided to make sure the user sees
     // the social provider login page
-    // Don't prompt for authentication if the idp is not provided (default behaviour)
-    prompt: idp ? 'login' : 'none',
+    // otherwise we'll use the prompt parameter provided
+    prompt: idp ? 'login' : prompt,
     // The sessionToken from authentication to exchange for session cookie
     sessionToken,
     // we send the generated stateParam as the state parameter
     state: authState.stateParam,
     // any scopes, by default the 'openid' scope is required
-    // the `guardian.identity-api.cookies.create.self.secure scope` is checked on IDAPI to return
-    // idapi cookies on authentication
-    scope: 'openid guardian.identity-api.cookies.create.self.secure',
+    scope: scopes.join(' '),
     // the redirect_uri is the url that we'll redirect the user to after
-    redirect_uri: ProfileOpenIdClientRedirectUris.WEB,
+    redirect_uri: redirectUri,
     // the identity provider if doing social login
     idp,
   });
