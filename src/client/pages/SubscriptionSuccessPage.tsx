5441e14d4612e46dbd49854e41790dca80689826
Tom Forbes <tom.forbes@theguardian.com>
2023-06-23 13:24:12 +0100

    useEffect

--- src/client/pages/SubscriptionSuccessPage.tsx
+++ src/client/pages/SubscriptionSuccessPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useClientState from '@/client/lib/hooks/useClientState';
 import { SubscriptionSuccess } from '@/client/pages/SubscriptionSuccess';
 import { SubscriptionAction } from '@/shared/lib/subscriptions';
@@ -13,15 +13,17 @@ export const SubscriptionSuccessPage = ({ action }: Props) => {
   const { pageData = {} } = clientState;
   const { returnUrl, accountManagementUrl, newsletterId } = pageData;
 
-  if (newsletterId) {
-    sendOphanComponentEvent({
-      action: 'SUBSCRIBE',
-      component: {
-        componentType: 'NEWSLETTER_SUBSCRIPTION',
-        id: newsletterId,
-      },
-    });
-  }
+  useEffect(() => {
+    if (newsletterId) {
+      sendOphanComponentEvent({
+        action: 'SUBSCRIBE',
+        component: {
+          componentType: 'NEWSLETTER_SUBSCRIPTION',
+          id: newsletterId,
+        },
+      });
+    }
+  }, [newsletterId]);
 
   return (
     <SubscriptionSuccess
