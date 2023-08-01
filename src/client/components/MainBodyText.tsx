bb1af205eea140a97e0d287b6fc269fba489619e
Mahesh Makani <mahesh@makani.dev>
2023-08-01 09:05:48 +0100

    feat(MainBodyText): add `marginTop` prop

--- src/client/components/MainBodyText.tsx
+++ src/client/components/MainBodyText.tsx
@@ -3,14 +3,19 @@ import { css, SerializedStyles } from '@emotion/react';
 import { space, text, textSans } from '@guardian/source-foundations';
 interface Props {
 	noMargin?: boolean;
+	marginTop?: boolean;
 	cssOverrides?: SerializedStyles;
 }
 
-const mainBodyTextStyles = (noMargin = false) => css`
+const mainBodyTextStyles = (noMargin = false, marginTop = false) => css`
 	${textSans.medium({ lineHeight: 'regular' })}
 	font-size: 17px;
 	color: ${text.primary};
-	margin-top: 0;
+	${marginTop
+		? ''
+		: css`
+				margin-top: 0;
+		  `}
 
 	${noMargin
 		? css`
@@ -25,6 +30,9 @@ export const MainBodyText = ({
 	children,
 	cssOverrides,
 	noMargin,
+	marginTop,
 }: PropsWithChildren<Props>) => (
-	<p css={[mainBodyTextStyles(noMargin), cssOverrides]}>{children}</p>
+	<p css={[mainBodyTextStyles(noMargin, marginTop), cssOverrides]}>
+		{children}
+	</p>
 );
