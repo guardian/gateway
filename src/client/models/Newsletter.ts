3ea5165e8a2231044531f2fd679a24a15e4a0bac
vanessa <32312712+vlbee@users.noreply.github.com>
2023-08-15 15:41:47 +0100

    Replace Opinion with Soccer newsletter for US

--- src/client/models/Newsletter.ts
+++ src/client/models/Newsletter.ts
@@ -1,14 +1,14 @@
 import { Newsletters } from '@/shared/model/Newsletter';
 import { palette } from '@guardian/source-foundations';
 
-const { brand, news, lifestyle, opinion } = palette;
+const { brand, news, lifestyle, sport } = palette;
 
 import {
 	DOWN_TO_EARTH_IMAGE,
 	FIRST_EDITION_UK_IMAGE,
 	THE_LONG_READ_IMAGE,
 	FIRST_THING_US_IMAGE,
-	OPINION_US_IMAGE,
+	SOCCER_US_IMAGE,
 	HEADLINES_US_IMAGE,
 	MORNING_MAIL_AU_IMAGE,
 	AFTERNOON_UPDATE_AU_IMAGE,
@@ -22,7 +22,7 @@ export const NEWSLETTER_IMAGES: Record<string, string> = {
 	[Newsletters.THE_LONG_READ]: THE_LONG_READ_IMAGE,
 	[Newsletters.HEADLINES_US]: HEADLINES_US_IMAGE,
 	[Newsletters.FIRST_THING_US]: FIRST_THING_US_IMAGE,
-	[Newsletters.OPINION_US]: OPINION_US_IMAGE,
+	[Newsletters.SOCCER_US]: SOCCER_US_IMAGE,
 	[Newsletters.MORNING_MAIL_AU]: MORNING_MAIL_AU_IMAGE,
 	[Newsletters.AFTERNOON_UPDATE_AU]: AFTERNOON_UPDATE_AU_IMAGE,
 	[Newsletters.FIVE_GREAT_READS_AU]: FIVE_GREAT_READS_AU_IMAGE,
@@ -33,6 +33,7 @@ export const NEWSLETTER_IMAGE_POSITIONS: Record<string, string> = {
 	[Newsletters.FIRST_EDITION_UK]: 'bottom left',
 	[Newsletters.DOWN_TO_EARTH]: 'bottom left',
 	[Newsletters.FIRST_THING_US]: 'bottom left',
+	[Newsletters.SOCCER_US]: 'bottom right',
 	[Newsletters.HEADLINES_US]: 'bottom left',
 	[Newsletters.MORNING_MAIL_AU]: 'bottom left',
 	[Newsletters.AFTERNOON_UPDATE_AU]: 'bottom left',
@@ -44,7 +45,7 @@ export const NEWSLETTER_COLOURS: Record<string, string> = {
 	[Newsletters.THE_LONG_READ]: brand[400],
 	[Newsletters.FIRST_THING_US]: news[400],
 	[Newsletters.HEADLINES_US]: news[400],
-	[Newsletters.OPINION_US]: opinion[400],
+	[Newsletters.SOCCER_US]: sport[400],
 	[Newsletters.MORNING_MAIL_AU]: news[400],
 	[Newsletters.AFTERNOON_UPDATE_AU]: news[400],
 	[Newsletters.FIVE_GREAT_READS_AU]: brand[400],
