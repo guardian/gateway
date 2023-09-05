a97c54793c824a93f8f2d4aac670839c46eb3d5d
vanessa <32312712+vlbee@users.noreply.github.com>
2023-09-05 14:51:20 +0100

    Set up Cypress retries in run mode

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	video: false,
 	chromeWebSecurity: false,
-	defaultCommandTimeout: 12000,
-	responseTimeout: 12000,
-	requestTimeout: 12000,
+	defaultCommandTimeout: 8000,
+	responseTimeout: 8000,
+	requestTimeout: 8000,
 	env: {
 		mockingEndpoint: 'localhost:9000/mock',
 	},
+	retries: { runMode: 2, openMode: 0 },
 	e2e: {
 		baseUrl: 'https://profile.thegulocal.com',
 		specPattern: 'cypress/integration/**/*.cy.ts',
