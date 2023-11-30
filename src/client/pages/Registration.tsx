9253a52edce70de1607e01299fcc9107fe4db3f7
Raphael Kabo <raphael.kabo@theguardian.com>
2023-11-30 11:27:11 +0000

    refactor: social buttons to add email button

--- src/client/pages/Registration.tsx
+++ src/client/pages/Registration.tsx
@@ -2,29 +2,13 @@ import React from 'react';
 import { QueryParams } from '@/shared/model/QueryParams';
 import { MainLayout } from '@/client/layouts/Main';
 import { generateSignInRegisterTabs } from '@/client/components/Nav';
-import { buildUrlWithQueryParams } from '@/shared/lib/routeUtils';
-import { SocialButtons } from '@/client/components/SocialButtons';
+import { AuthProviderButtons } from '@/client/components/AuthProviderButtons';
 import { usePageLoadOphanInteraction } from '@/client/lib/hooks/usePageLoadOphanInteraction';
 import {
 	GuardianTerms,
 	JobsTerms,
 	termsContainer,
 } from '@/client/components/Terms';
-import { LinkButton, SvgEnvelope } from '@guardian/source-react-components';
-import { css } from '@emotion/react';
-import { space } from '@guardian/source-foundations';
-
-const emailButton = css`
-	width: 100%;
-	justify-content: center;
-`;
-
-const registrationButtons = css`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: ${space[3]}px;
-`;
 
 export type RegistrationProps = {
 	email?: string;
@@ -56,21 +40,12 @@ export const Registration = ({ queryParams }: RegistrationProps) => {
 	return (
 		<MainLayout tabs={tabs}>
 			<RegistrationTerms isJobs={isJobs} />
-			<div css={registrationButtons}>
-				<SocialButtons
-					queryParams={queryParams}
-					marginTop={true}
-					context="Sign up"
-				/>
-				<LinkButton
-					icon={<SvgEnvelope />}
-					cssOverrides={emailButton}
-					priority="tertiary"
-					href={buildUrlWithQueryParams('/register/email', {}, queryParams)}
-				>
-					Sign up with email
-				</LinkButton>
-			</div>
+			<AuthProviderButtons
+				queryParams={queryParams}
+				marginTop={true}
+				context="Sign up"
+				providers={['social', 'email']}
+			/>
 		</MainLayout>
 	);
 };
