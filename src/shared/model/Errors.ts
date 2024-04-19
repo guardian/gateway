2a371d9ad8667d6b2ae51c638dd7559d46e2aa26
Raphael Kabo <raphael.kabo@theguardian.com>
2024-04-19 12:52:43 +0100

    feat: new error messages for profile provisioning failure

--- src/shared/model/Errors.ts
+++ src/shared/model/Errors.ts
@@ -34,6 +34,7 @@ export enum SignInErrors {
 export enum RegistrationErrors {
 	GENERIC = 'There was a problem registering, please try again.',
 	EMAIL_INVALID = 'Please enter a valid email address.',
+	PROVISIONING_FAILURE = 'Your account has been created but there was a problem signing you in.',
 }
 
 // shown at the top of the change password page when something goes wrong
