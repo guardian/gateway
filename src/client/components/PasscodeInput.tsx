9ab48d38a5e02090e67d755ea77fef97707eecf1
Mahesh Makani <mahesh@makani.dev>
2024-06-24 11:23:34 +0100

    fix(PasscodeInput): validate input using client side javascript

--- src/client/components/PasscodeInput.tsx
+++ src/client/components/PasscodeInput.tsx
@@ -1,6 +1,6 @@
+import React, { useState } from 'react';
 import { FieldError } from '@/shared/model/ClientState';
 import { TextInputProps } from '@guardian/source/react-components';
-import React from 'react';
 import { css } from '@emotion/react';
 import ThemedTextInput from '@/client/components/ThemedTextInput';
 
@@ -14,9 +14,44 @@ const passcodeInputStyles = css`
 `;
 
 export const PasscodeInput = ({
-	passcode,
+	passcode = '',
 	fieldErrors,
 }: PasscodeInputProps) => {
+	/**
+	 * In gateway we normally avoid using client side javascript, but in this case
+	 * we allow it as it an enhancement to the user experience and not required for
+	 * the form to work.
+	 */
+
+	// This function removes all non-numeric characters from the input
+	const sanitiseCode = (code: string) => {
+		return code.replace(/\D*/g, '');
+	};
+
+	// Using an object to store the input value so that we re-render the component
+	// whenever there is an user input, rather than only when the state changes
+	const [input, setInput] = useState({ value: sanitiseCode(passcode) });
+
+	// Update the input value on user input
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInput({
+			value: sanitiseCode(e.target.value),
+		});
+	};
+
+	// Update the input value on paste
+	const onPaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+		const paste = e.clipboardData.getData('text/plain');
+
+		if (!paste) {
+			return;
+		}
+
+		setInput({
+			value: sanitiseCode(paste),
+		});
+	};
+
 	return (
 		<div>
 			<ThemedTextInput
@@ -30,6 +65,9 @@ export const PasscodeInput = ({
 				error={fieldErrors?.find((error) => error.field === 'code')?.message}
 				defaultValue={passcode}
 				cssOverrides={passcodeInputStyles}
+				value={input.value}
+				onChange={onChange}
+				onPaste={onPaste}
 			/>
 		</div>
 	);
