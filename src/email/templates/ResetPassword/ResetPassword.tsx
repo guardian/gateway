aad0cdb95f8522cde2826ff2b1cb3fd08b14723b
Nick Hart <126492147+nickhartyyy@users.noreply.github.com>
2024-07-17 16:03:21 +0100

    Password reset email update
    We need to make it clear to readers that they can retry resetting their password if the link has expired. Many readers contact User Help to say their link has expired, but they don’t realise they can trigger a new one themselves. This change hopes to solve that problem.
    
    Signed-off-by: Nick Hart <126492147+nickhartyyy@users.noreply.github.com>

--- src/email/templates/ResetPassword/ResetPassword.tsx
+++ src/email/templates/ResetPassword/ResetPassword.tsx
@@ -6,6 +6,7 @@ import { Header } from '@/email/components/Header';
 import { SubHeader } from '@/email/components/SubHeader';
 import { Text } from '@/email/components/Text';
 import { Footer } from '@/email/components/Footer';
+import { Link } from '@/email/components/Link';
 
 export const ResetPassword = () => {
 	return (
@@ -16,7 +17,13 @@ export const ResetPassword = () => {
 			<Text>
 				You&apos;ve asked us to send you a link to reset your password.
 			</Text>
-			<Text noPaddingBottom>This link is valid for 60 minutes.</Text>
+			<Text noPaddingBottom>
+				This link is valid for 60 minutes. If your link has expired, please try{' '}
+				<Link href="https://profile.theguardian.com/reset-password">
+					resetting your password again
+				</Link>
+				.
+			</Text>
 			<Button href={'$passwordResetLink'}>Reset password</Button>
 			<Footer
 				mistakeParagraphComponent={
