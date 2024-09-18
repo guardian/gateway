385128c905b3c80177df5d6b5306c5138ff3cd62
Mahesh Makani <mahesh@makani.dev>
2024-09-18 14:55:21 +0100

    test(ete-okta): add tests for passcode reset password for non-ACTIVE user states

--- cypress/integration/ete/reset_password_passcode.7.cy.ts
+++ cypress/integration/ete/reset_password_passcode.7.cy.ts
@@ -1,3 +1,4 @@
+import { Status } from '../../../src/server/models/okta/User';
 import {
 	randomMailosaurEmail,
 	randomPassword,
@@ -441,6 +442,318 @@ describe('Password reset recovery flows - with Passcodes', () => {
 					});
 				},
 			);
+		});
+	});
+
+	context('STAGED user', () => {
+		it('allows the user to change their password - STAGED - No Passcode Verified', () => {
+			const emailAddress = randomMailosaurEmail();
+			cy.visit(`/register/email`);
+
+			const timeRequestWasMade = new Date();
+			cy.get('input[name=email]').type(emailAddress);
+			cy.get('[data-cy="main-form-submit-button"]').click();
+
+			cy.contains('Enter your code');
+			cy.contains(emailAddress);
+
+			cy.checkForEmailAndGetDetails(emailAddress, timeRequestWasMade).then(
+				({ body, codes }) => {
+					// email
+					expect(body).to.have.string('Your verification code');
+					expect(codes?.length).to.eq(1);
+					const code = codes?.[0].value;
+					expect(code).to.match(/^\d{6}$/);
+
+					// passcode page
+					cy.url().should('include', '/register/email-sent');
+
+					cy.getTestOktaUser(emailAddress).then((oktaUser) => {
+						expect(oktaUser.status).to.eq(Status.STAGED);
+						// make sure we don't use a passcode
+						// we instead reset their password using passcodes
+						cy.visit('/reset-password?usePasscodesResetPassword=true');
+
+						const timeRequestWasMade = new Date();
+						cy.contains('Reset password');
+						cy.get('[data-cy="main-form-submit-button"]').click();
+
+						cy.checkForEmailAndGetDetails(
+							emailAddress,
+							timeRequestWasMade,
+						).then(({ body, codes }) => {
+							// email
+							expect(body).to.have.string('Your verification code');
+							expect(codes?.length).to.eq(1);
+							const code = codes?.[0].value;
+							expect(code).to.match(/^\d{6}$/);
+
+							// passcode page
+							cy.url().should('include', '/reset-password/email-sent');
+							cy.contains('Enter your one-time code');
+
+							cy.get('input[name=code]').clear().type(code!);
+							cy.contains('Submit one-time code').click();
+
+							cy.url().should('contain', '/set-password');
+
+							cy.get('input[name="password"]').type(randomPassword());
+							cy.get('button[type="submit"]').click();
+
+							cy.url().should('contain', '/set-password/complete');
+						});
+					});
+				},
+			);
+		});
+
+		it('allows the user to change their password - STAGED - Passcode Verified - No Password', () => {
+			const emailAddress = randomMailosaurEmail();
+			cy.visit(`/register/email`);
+
+			const timeRequestWasMade = new Date();
+			cy.get('input[name=email]').type(emailAddress);
+			cy.get('[data-cy="main-form-submit-button"]').click();
+
+			cy.contains('Enter your code');
+			cy.contains(emailAddress);
+
+			cy.checkForEmailAndGetDetails(emailAddress, timeRequestWasMade).then(
+				({ body, codes }) => {
+					// email
+					expect(body).to.have.string('Your verification code');
+					expect(codes?.length).to.eq(1);
+					const code = codes?.[0].value;
+					expect(code).to.match(/^\d{6}$/);
+
+					// passcode page, use passcode
+					cy.url().should('include', '/register/email-sent');
+					cy.get('input[name=code]').type(code!);
+					cy.contains('Submit verification code').click();
+
+					// password page, don't set password
+					cy.url().should('include', '/welcome/password');
+
+					cy.getTestOktaUser(emailAddress).then((oktaUser) => {
+						expect(oktaUser.status).to.eq(Status.STAGED);
+
+						// redirect to reset password with passcodes
+						cy.visit('/reset-password?usePasscodesResetPassword=true');
+
+						const timeRequestWasMade = new Date();
+						cy.contains('Reset password');
+						cy.get('[data-cy="main-form-submit-button"]').click();
+
+						cy.checkForEmailAndGetDetails(
+							emailAddress,
+							timeRequestWasMade,
+						).then(({ body, codes }) => {
+							// email
+							expect(body).to.have.string('Your one-time passcode');
+							expect(codes?.length).to.eq(1);
+							const code = codes?.[0].value;
+							expect(code).to.match(/^\d{6}$/);
+
+							// passcode page
+							cy.url().should('include', '/reset-password/email-sent');
+							cy.contains('Enter your one-time code');
+
+							cy.get('input[name=code]').clear().type(code!);
+							cy.contains('Submit one-time code').click();
+
+							// password page
+							cy.url().should('include', '/reset-password/password');
+
+							cy.get('input[name="password"]').type(randomPassword());
+							cy.get('button[type="submit"]').click();
+
+							// password complete page
+							cy.url().should('include', '/reset-password/complete');
+
+							cy.contains('Password updated');
+						});
+					});
+				},
+			);
+		});
+
+		it('allows the user to change their password - STAGED - Created via Classic API (i.e guest user)', () => {
+			cy.createTestUser({ isGuestUser: true })?.then(({ emailAddress }) => {
+				cy.getTestOktaUser(emailAddress).then((oktaUser) => {
+					expect(oktaUser.status).to.eq(Status.STAGED);
+					cy.visit('/reset-password?usePasscodesResetPassword=true');
+
+					const timeRequestWasMade = new Date();
+					cy.get('input[name=email]').type(emailAddress);
+					cy.get('[data-cy="main-form-submit-button"]').click();
+
+					cy.checkForEmailAndGetDetails(emailAddress, timeRequestWasMade).then(
+						({ body, codes }) => {
+							// email
+							expect(body).to.have.string('Your one-time passcode');
+							expect(codes?.length).to.eq(1);
+							const code = codes?.[0].value;
+							expect(code).to.match(/^\d{6}$/);
+
+							// passcode page
+							cy.url().should('include', '/reset-password/email-sent');
+							cy.contains('Enter your one-time code');
+
+							cy.get('input[name=code]').clear().type(code!);
+							cy.contains('Submit one-time code').click();
+
+							// password page
+							cy.url().should('include', '/reset-password/password');
+
+							cy.get('input[name="password"]').type(randomPassword());
+							cy.get('button[type="submit"]').click();
+
+							// password complete page
+							cy.url().should('include', '/reset-password/complete');
+
+							cy.contains('Password updated');
+						},
+					);
+				});
+			});
+		});
+	});
+
+	context('PROVISIONED user', () => {
+		it('allows the user to change their password - PROVISIONED', () => {
+			cy.createTestUser({ isGuestUser: true })?.then(({ emailAddress }) => {
+				cy.activateTestOktaUser(emailAddress).then(() => {
+					cy.getTestOktaUser(emailAddress).then((oktaUser) => {
+						expect(oktaUser.status).to.eq(Status.PROVISIONED);
+						cy.visit('/reset-password?usePasscodesResetPassword=true');
+
+						const timeRequestWasMade = new Date();
+						cy.get('input[name=email]').type(emailAddress);
+						cy.get('[data-cy="main-form-submit-button"]').click();
+
+						cy.checkForEmailAndGetDetails(
+							emailAddress,
+							timeRequestWasMade,
+						).then(({ body, codes }) => {
+							// email
+							expect(body).to.have.string('Your one-time passcode');
+							expect(codes?.length).to.eq(1);
+							const code = codes?.[0].value;
+							expect(code).to.match(/^\d{6}$/);
+
+							// passcode page
+							cy.url().should('include', '/reset-password/email-sent');
+							cy.contains('Enter your one-time code');
+
+							cy.get('input[name=code]').clear().type(code!);
+							cy.contains('Submit one-time code').click();
+
+							// password page
+							cy.url().should('include', '/reset-password/password');
+
+							cy.get('input[name="password"]').type(randomPassword());
+							cy.get('button[type="submit"]').click();
+
+							// password complete page
+							cy.url().should('include', '/reset-password/complete');
+
+							cy.contains('Password updated');
+						});
+					});
+				});
+			});
+		});
+	});
+
+	context('RECOVERY user', () => {
+		it('allows the user to change their password - RECOVERY', () => {
+			cy.createTestUser({ isGuestUser: false })?.then(({ emailAddress }) => {
+				cy.resetOktaUserPassword(emailAddress).then(() => {
+					cy.getTestOktaUser(emailAddress).then((oktaUser) => {
+						expect(oktaUser.status).to.eq(Status.RECOVERY);
+						cy.visit('/reset-password?usePasscodesResetPassword=true');
+
+						const timeRequestWasMade = new Date();
+						cy.get('input[name=email]').type(emailAddress);
+						cy.get('[data-cy="main-form-submit-button"]').click();
+
+						cy.checkForEmailAndGetDetails(
+							emailAddress,
+							timeRequestWasMade,
+						).then(({ body, codes }) => {
+							// email
+							expect(body).to.have.string('Your one-time passcode');
+							expect(codes?.length).to.eq(1);
+							const code = codes?.[0].value;
+							expect(code).to.match(/^\d{6}$/);
+
+							// passcode page
+							cy.url().should('include', '/reset-password/email-sent');
+							cy.contains('Enter your one-time code');
+
+							cy.get('input[name=code]').clear().type(code!);
+							cy.contains('Submit one-time code').click();
+
+							// password page
+							cy.url().should('include', '/reset-password/password');
+
+							cy.get('input[name="password"]').type(randomPassword());
+							cy.get('button[type="submit"]').click();
+
+							// password complete page
+							cy.url().should('include', '/reset-password/complete');
+
+							cy.contains('Password updated');
+						});
+					});
+				});
+			});
+		});
+	});
+
+	context('PASSWORD_EXPIRED user', () => {
+		it('allows the user to change their password - PASSWORD_EXPIRED', () => {
+			cy.createTestUser({ isGuestUser: false })?.then(({ emailAddress }) => {
+				cy.expireOktaUserPassword(emailAddress).then(() => {
+					cy.getTestOktaUser(emailAddress).then((oktaUser) => {
+						expect(oktaUser.status).to.eq(Status.PASSWORD_EXPIRED);
+						cy.visit('/reset-password?usePasscodesResetPassword=true');
+
+						const timeRequestWasMade = new Date();
+						cy.get('input[name=email]').type(emailAddress);
+						cy.get('[data-cy="main-form-submit-button"]').click();
+
+						cy.checkForEmailAndGetDetails(
+							emailAddress,
+							timeRequestWasMade,
+						).then(({ body, codes }) => {
+							// email
+							expect(body).to.have.string('Your one-time passcode');
+							expect(codes?.length).to.eq(1);
+							const code = codes?.[0].value;
+							expect(code).to.match(/^\d{6}$/);
+
+							// passcode page
+							cy.url().should('include', '/reset-password/email-sent');
+							cy.contains('Enter your one-time code');
+
+							cy.get('input[name=code]').clear().type(code!);
+							cy.contains('Submit one-time code').click();
+
+							// password page
+							cy.url().should('include', '/reset-password/password');
+
+							cy.get('input[name="password"]').type(randomPassword());
+							cy.get('button[type="submit"]').click();
+
+							// password complete page
+							cy.url().should('include', '/reset-password/complete');
+
+							cy.contains('Password updated');
+						});
+					});
+				});
+			});
 		});
 	});
 });
