68d86cfa07c27bad551eccd443af675ae6ad1a99
Mahesh Makani <mahesh@makani.dev>
2024-10-16 09:59:30 +0100

    feat: add `SignInPasscodeEmailSentPage` component

---
+++ src/client/pages/SignInPasscodeEmailSentPage.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import useClientState from '@/client/lib/hooks/useClientState';
+import { buildQueryParamsString } from '@/shared/lib/queryParams';
+import { buildUrl } from '@/shared/lib/routeUtils';
+import { PasscodeEmailSent } from '@/client/pages/PasscodeEmailSent';
+import { SignedInAsPage } from './SignedInAsPage';
+
+export const SignInPasscodeEmailSentPage = () => {
+	const clientState = useClientState();
+	const {
+		pageData = {},
+		queryParams,
+		globalMessage = {},
+		recaptchaConfig,
+		shortRequestId,
+	} = clientState;
+	const { email, fieldErrors, token, passcodeUsed } = pageData;
+	const { emailSentSuccess } = queryParams;
+	const { error } = globalMessage;
+	const { recaptchaSiteKey } = recaptchaConfig;
+
+	const queryString = buildQueryParamsString(queryParams, {
+		emailSentSuccess: true,
+	});
+
+	// if the passcode has already been used, show the signed in as page
+	if (passcodeUsed) {
+		return <SignedInAsPage />;
+	}
+
+	return (
+		<PasscodeEmailSent
+			email={email}
+			queryString={queryString}
+			changeEmailPage={buildUrl('/signin')}
+			passcodeAction={buildUrl('/signin/code')}
+			showSuccess={emailSentSuccess}
+			errorMessage={error}
+			recaptchaSiteKey={recaptchaSiteKey}
+			formTrackingName="signin-passcode-resend"
+			fieldErrors={fieldErrors}
+			passcode={token}
+			expiredPage={buildUrl('/signin/code/expired')}
+			noAccountInfo
+			textType="generic"
+			shortRequestId={shortRequestId}
+		/>
+	);
+};
