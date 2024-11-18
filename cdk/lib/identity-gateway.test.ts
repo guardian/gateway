96243ce06c2661135aa5ab3d489d0179f91f836c
Ashleigh Carr <ashcorr20@gmail.com>
2024-11-18 17:09:16 +0000

    feat: Fully switch to GuCDK instead of YAML.

--- cdk/lib/identity-gateway.test.ts
+++ cdk/lib/identity-gateway.test.ts
@@ -1,12 +1,22 @@
-import { App } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import { IdentityGateway } from "./identity-gateway";
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { IdentityGateway } from './identity-gateway';
 
-describe("The IdentityGateway stack", () => {
-  it("matches the snapshot", () => {
-    const app = new App();
-    const stack = new IdentityGateway(app, "IdentityGateway", { stack: "identity", stage: "TEST" });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
-  });
+describe('The IdentityGateway stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new IdentityGateway(app, 'IdentityGateway', {
+			stack: 'identity',
+			stage: 'TEST',
+			env: { region: 'eu-west-1' },
+			scaling: {
+				minimumInstances: 1,
+				maximumInstances: 2,
+			},
+			baseUri: `profile.code.dev-theguardian.com`,
+			defaultReturnUri: `https://m.code.dev-theguardian.com`,
+		});
+		const template = Template.fromStack(stack);
+		expect(template.toJSON()).toMatchSnapshot();
+	});
 });
