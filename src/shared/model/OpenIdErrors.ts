505ddcc4eac883df485f2ed73a66a579e79b170e
Mahesh Makani <mahesh@makani.dev>
2025-03-04 12:26:48 +0000

    refactor(typescript): use `erasableSyntaxOnly` flag for typechecking
    In TypeScript 5.8 a `tsc` flag was introduced called [`--erasableSyntaxOnly`](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-8.html#the---erasablesyntaxonly-option).
    
    When this flag is enabled, TypeScript will error on most TypeScript-specific constructs that affect runtime behaviour.
    
    This means things like `enum` declarations, `namespace`s and `module`s with runtime code, parameter properties in classes, and non-ECMAScript `import =` and `export =` assignments don't work.
    
    This is useful as it it allows for a step towards ["type stripping"](https://nodejs.org/api/typescript.html#type-stripping) which allows runtimes like nodejs to run typescript files by simply stripping any inline types without performing type checking or any other code transformation. Essentially allowing the runtimes to run typescript files without any build/transpilation steps!
    
    The more immediate benefit for us is to enable developers to use typescript as a simple layer over javascript, rather than relying on typescript code that outputs generated code at compilaton such as enums. This should improve the developer experience and avoid common pitalls. By stripping types, it means the JavaScript output is as similar as possible to the originally authored TypeScript code.
    
    See https://satanacchio.hashnode.dev/everything-you-need-to-know-about-nodejs-type-stripping as a useful blog post talking about this.

--- src/shared/model/OpenIdErrors.ts
+++ src/shared/model/OpenIdErrors.ts
@@ -2,12 +2,13 @@
  * Possible `error` types return by OpenID Connect and social login flows with okta
  * Sourced from https://developer.okta.com/docs/reference/error-codes/#example-errors-for-openid-connect-and-social-login
  */
-export enum OpenIdErrors {
-	ACCESS_DENIED = 'access_denied',
-	LOGIN_REQUIRED = 'login_required',
-}
+export const OpenIdErrors = {
+	ACCESS_DENIED: 'access_denied',
+	LOGIN_REQUIRED: 'login_required',
+} as const;
 
-export enum OpenIdErrorDescriptions {
-	ACCOUNT_LINKING_DENIED_GROUPS = 'User linking was denied because the user is not in any of the specified groups.',
-	USER_STATUS_INVALID = 'User status is invalid.',
-}
+export const OpenIdErrorDescriptions = {
+	ACCOUNT_LINKING_DENIED_GROUPS:
+		'User linking was denied because the user is not in any of the specified groups.',
+	USER_STATUS_INVALID: 'User status is invalid.',
+} as const;
