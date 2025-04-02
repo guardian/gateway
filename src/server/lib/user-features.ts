546cdcbcb8913857a493c5c30b830c0a52f28ed7
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-04-02 10:27:35 +0100

    Update zod object and implement.

--- src/server/lib/user-features.ts
+++ src/server/lib/user-features.ts
@@ -2,12 +2,18 @@ import { Response } from 'express';
 import { getConfiguration } from '@/server/lib/getConfiguration';
 import { getUserAttributes } from '@/server/lib/members-data-api/user-attributes';
 import { getUserBenefits } from './user-benefits-api/user-benefits';
+import { UserBenefitsSchema } from '@/shared/lib/user-benefits-api';
 
 // port of some functionality from https://github.com/guardian/frontend/blob/main/static/src/javascripts/projects/common/modules/commercial/user-features.ts
 
 const { baseUri } = getConfiguration();
 
 const domain = `${baseUri.replace('profile.', '').split(':')[0]}`;
+
+export const AD_FREE_USER_COOKIE = 'GU_AF1';
+export const HIDE_SUPPORT_MESSAGING_COOKIE = 'gu_hide_support_messaging';
+export const ALLOW_REJECT_ALL_COOKIE = 'gu_allow_reject_all';
+export const USER_BENEFITS_EXPIRY_COOKIE = 'gu_user_benefits_expiry';
 
 /**
  * @name setUserFeatureCookies
@@ -34,11 +40,12 @@ export const setUserFeatureCookies = async ({
 		accessToken,
 	});
 
-	if (userBenefits?.benefits) {
-		// for testing purposes
-		res.cookie('gu_test_cookie', 'true', {
-			domain,
-			expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+	console.log('userBenefits', userBenefits);
+
+	if (userBenefits) {
+		persistUserBenefitsCookies({
+			userBenefits,
+			res,
 		});
 	}
 
@@ -53,5 +60,36 @@ export const setUserFeatureCookies = async ({
 			domain,
 			expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
 		});
+	}
+};
+
+export const createCookie = ({
+	name,
+	res,
+}: {
+	name: string;
+	res: Response;
+}) => {
+	res.cookie(name, 'true', {
+		domain,
+		expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+	});
+};
+
+export const persistUserBenefitsCookies = ({
+	userBenefits,
+	res,
+}: {
+	userBenefits: UserBenefitsSchema;
+	res: Response;
+}) => {
+	if (userBenefits.hideSupportMessaging) {
+		createCookie({ name: HIDE_SUPPORT_MESSAGING_COOKIE, res });
+	}
+	if (userBenefits.allowRejectAll) {
+		createCookie({ name: ALLOW_REJECT_ALL_COOKIE, res });
+	}
+	if (userBenefits.adFree) {
+		createCookie({ name: AD_FREE_USER_COOKIE, res });
 	}
 };
--- src/shared/lib/user-benefits-api.ts
+++ src/shared/lib/user-benefits-api.ts
@@ -1,8 +1,21 @@
 import { z } from 'zod';
 
+// export const userBenefitsResponseSchema = z.object({
+// 	benefits: z
+// 		.array(z.enum(['adFree', 'allowRejectAll', 'hideSupportMessaging']))
+// 		.optional(),
+// });
+
 export const userBenefitsResponseSchema = z.object({
-	benefits: z.array(z.string()),
-	trials: z.object({}).optional(),
+	benefits: z.array(z.string()).optional(),
 });
+
+export const userBenefitsSchema = z.object({
+	adFree: z.boolean().optional(),
+	allowRejectAll: z.boolean().optional(),
+	hideSupportMessaging: z.boolean().optional(),
+});
+
+export type UserBenefitsSchema = z.infer<typeof userBenefitsSchema>;
 
 export type UserBenefitsResponse = z.infer<typeof userBenefitsResponseSchema>;
