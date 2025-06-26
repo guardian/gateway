a591f74a5b82e329197c8afe80733d82085edfad
AshCorr <ashcorr20@gmail.com>
2025-06-26 13:01:17 +0100

    feat(cdk): Use latest standard Guardian CDK setup

--- cdk/lib/identity-gateway.test.ts
+++ cdk/lib/identity-gateway.test.ts
@@ -1,12 +1,15 @@
-import { App } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import { IdentityGateway } from "./identity-gateway";
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { IdentityGateway } from './identity-gateway';
 
-describe("The IdentityGateway stack", () => {
-  it("matches the snapshot", () => {
-    const app = new App();
-    const stack = new IdentityGateway(app, "IdentityGateway", { stack: "identity", stage: "TEST" });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
-  });
+describe('The IdentityGateway stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new IdentityGateway(app, 'IdentityGateway', {
+			stack: 'identity',
+			stage: 'TEST',
+		});
+		const template = Template.fromStack(stack);
+		expect(template.toJSON()).toMatchSnapshot();
+	});
 });
