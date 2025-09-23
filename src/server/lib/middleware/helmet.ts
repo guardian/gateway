d00a89784f77747fb7ee1010a3613fb9dc058f78
Akinsola Lawanson <akinsola.lawanson@guardian.co.uk>
2025-09-23 15:38:02 +0100

    Add Support to frameAncestors CSP directive in order to allow the iframing of gateway in Support-Frontend

--- src/server/lib/middleware/helmet.ts
+++ src/server/lib/middleware/helmet.ts
@@ -23,6 +23,15 @@ const CSP_VALID_URI = {
 
 const idapiOrigin = idapiBaseUrl.replace(/https?:\/\/|\/identity-api/g, '');
 
+// Dynamically build frame-ancestors based on stage.
+// This builds an array of urls that are appropriate for the current stage
+// Filter removes any falsey values
+const frameAncestors = [
+	stage === 'PROD' && 'https://support.theguardian.com',
+	stage === 'CODE' && 'https://support.code.dev-theguardian.com',
+	stage === 'DEV' && 'support.thegulocal.com',
+].filter((element) => !!element) as string[];
+
 const scriptSrc = [
 	`${baseUri}`,
 	CSP_VALID_URI.GOOGLE_RECAPTCHA,
@@ -40,7 +49,7 @@ const helmetConfig: HelmetOptions = {
 		directives: {
 			baseUri: [HELMET_OPTIONS.NONE],
 			defaultSrc: [HELMET_OPTIONS.NONE],
-			frameAncestors: [HELMET_OPTIONS.NONE],
+			frameAncestors,
 			styleSrc: [HELMET_OPTIONS.UNSAFE_INLINE],
 			scriptSrc,
 			imgSrc: [
