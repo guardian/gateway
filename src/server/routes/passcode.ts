c7e8222f260fd8abe35f4b4b23d44a6105eee201
Richard Bangay <richard.bangay@guardian.co.uk>
2025-10-20 10:02:42 +0100

    passcode.ts tidy up; change order of 'handleAsyncErrors' switch case statements, refactor large if else block to be a series of ifs with returns to hopefully make the code more readable

--- src/server/routes/passcode.ts
+++ src/server/routes/passcode.ts
@@ -20,102 +20,11 @@ import handleRecaptcha from '../lib/recaptcha';
 router.get('/passcode', (req: Request, res: ResponseWithRequestState) => {
 	const state = res.locals;
 	const encrypedCookieState = readEncryptedStateCookie(req);
+	const email = encrypedCookieState?.email;
 	if (!encrypedCookieState?.email) {
 		return res.send(403);
 	}
-	const email = encrypedCookieState.email;
-	if (encrypedCookieState?.stateHandle) {
-		try {
-			const queryParams = state.queryParams;
-			const { error, error_description } = queryParams;
-
-			const possibleError = getErrorMessageFromQueryParams(
-				error,
-				error_description,
-			);
-
-			if (encrypedCookieState.passcodeUsed) {
-				switch (encrypedCookieState.signInOrRegister) {
-					case 'SIGNIN': {
-						const { baseUri } = getConfiguration();
-						const continueLink =
-							queryParams.clientId === 'jobs'
-								? `https://${baseUri}${addQueryParamsToPath(
-										'/agree/GRS',
-										queryParams,
-									)}`
-								: queryParams.fromURI ||
-									`https://${baseUri}${addQueryParamsToPath(
-										'/signin/refresh',
-										queryParams,
-									)}`;
-						// the "Sign in" link is used to log in or register as a different user, so we add the parameters we need to the link
-						const signInLink = encodeURIComponent(
-							`https://${baseUri}${addQueryParamsToPath('/signin', state.queryParams)}`,
-						);
-
-						// the sign out link is what's the "Sign in with a different email" link is pointing to
-						// it signs the user out first, and then redirects to the sign in page with the required parameters
-						const signOutLink = addQueryParamsToPath('/signout', {
-							returnUrl: signInLink,
-						});
-						const html = renderer('/signed-in-as', {
-							requestState: mergeRequestState(state, {
-								pageData: {
-									email,
-									continueLink,
-									signOutLink,
-								},
-								globalMessage: {
-									error: possibleError,
-								},
-							}),
-							pageTitle: 'Sign in',
-						});
-						res.type('html').send(html);
-						break;
-					}
-					case 'REGISTER': {
-						const html = renderer('/passcode-used-register', {
-							requestState: mergeRequestState(state, {
-								pageData: {
-									email,
-								},
-								globalMessage: {
-									error: possibleError,
-								},
-							}),
-							pageTitle: 'Check Your Inbox',
-						});
-						res.type('html').send(html);
-					}
-				}
-			} else {
-				const html = renderer('/passcode', {
-					requestState: mergeRequestState(state, {
-						pageData: {
-							email,
-							timeUntilTokenExpiry: convertExpiresAtToExpiryTimeInMs(
-								encrypedCookieState.stateHandleExpiresAt,
-							),
-						},
-						queryParams: {
-							...res.locals.queryParams,
-							emailSentSuccess: true,
-						},
-						globalMessage: {
-							error: possibleError,
-						},
-					}),
-					pageTitle: 'Check Your Inbox',
-				});
-				res.type('html').send(html);
-			}
-		} catch (error) {
-			logger.error(`${req.method} ${req.originalUrl} Error`, error);
-			res.redirect(303, addQueryParamsToPath('/signin', state.queryParams));
-		}
-	} else {
+	if (!encrypedCookieState?.stateHandle) {
 		const html = renderer('/passcode/resend', {
 			requestState: mergeRequestState(state, {
 				pageData: {
@@ -126,7 +35,100 @@ router.get('/passcode', (req: Request, res: ResponseWithRequestState) => {
 			}),
 			pageTitle: 'Check Your Inbox',
 		});
-		res.type('html').send(html);
+		return res.type('html').send(html);
+	}
+	try {
+		const queryParams = state.queryParams;
+		const { error, error_description } = queryParams;
+
+		const possibleError = getErrorMessageFromQueryParams(
+			error,
+			error_description,
+		);
+
+		if (
+			encrypedCookieState.passcodeUsed &&
+			encrypedCookieState.signInOrRegister === 'SIGNIN'
+		) {
+			const { baseUri } = getConfiguration();
+			const continueLink =
+				queryParams.clientId === 'jobs'
+					? `https://${baseUri}${addQueryParamsToPath(
+							'/agree/GRS',
+							queryParams,
+						)}`
+					: queryParams.fromURI ||
+						`https://${baseUri}${addQueryParamsToPath(
+							'/signin/refresh',
+							queryParams,
+						)}`;
+			// the "Sign in" link is used to log in or register as a different user, so we add the parameters we need to the link
+			const signInLink = encodeURIComponent(
+				`https://${baseUri}${addQueryParamsToPath('/signin', state.queryParams)}`,
+			);
+
+			// the sign out link is what's the "Sign in with a different email" link is pointing to
+			// it signs the user out first, and then redirects to the sign in page with the required parameters
+			const signOutLink = addQueryParamsToPath('/signout', {
+				returnUrl: signInLink,
+			});
+			const html = renderer('/signed-in-as', {
+				requestState: mergeRequestState(state, {
+					pageData: {
+						email,
+						continueLink,
+						signOutLink,
+					},
+					globalMessage: {
+						error: possibleError,
+					},
+				}),
+				pageTitle: 'Sign in',
+			});
+			return res.type('html').send(html);
+		}
+		if (
+			encrypedCookieState.passcodeUsed &&
+			encrypedCookieState.signInOrRegister === 'REGISTER'
+		) {
+			const html = renderer('/passcode-used-register', {
+				requestState: mergeRequestState(state, {
+					pageData: {
+						email,
+					},
+					globalMessage: {
+						error: possibleError,
+					},
+				}),
+				pageTitle: 'Check Your Inbox',
+			});
+			return res.type('html').send(html);
+		}
+
+		if (!encrypedCookieState.passcodeUsed) {
+			const html = renderer('/passcode', {
+				requestState: mergeRequestState(state, {
+					pageData: {
+						email,
+						timeUntilTokenExpiry: convertExpiresAtToExpiryTimeInMs(
+							encrypedCookieState.stateHandleExpiresAt,
+						),
+					},
+					queryParams: {
+						...res.locals.queryParams,
+						emailSentSuccess: true,
+					},
+					globalMessage: {
+						error: possibleError,
+					},
+				}),
+				pageTitle: 'Check Your Inbox',
+			});
+			return res.type('html').send(html);
+		}
+	} catch (error) {
+		logger.error(`${req.method} ${req.originalUrl} Error`, error);
+		res.redirect(303, addQueryParamsToPath('/signin', state.queryParams));
 	}
 });
 
@@ -135,11 +137,11 @@ router.post(
 	handleAsyncErrors(async (req: Request, res: ResponseWithRequestState) => {
 		const encrypedCookieState = readEncryptedStateCookie(req);
 		switch (encrypedCookieState?.signInOrRegister) {
+			case 'REGISTER':
+				return await registerPasscodeHandler(req, res);
 			case 'SIGNIN':
 			default:
 				return await oktaIdxApiSubmitPasscodeController({ req, res });
-			case 'REGISTER':
-				return await registerPasscodeHandler(req, res);
 		}
 	}),
 );
