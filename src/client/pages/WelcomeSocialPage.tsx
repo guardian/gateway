8d5b7f83b376345771e5ecb2c9c641419fea61f3
Ioanna Kokkini <ioannakok@hotmail.com>
2026-01-23 10:20:56 +0000

    Add `welcome/google-one-tap` route
    Co-authored-by: AshCorr <ashcorr20@gmail.com>
    
    In order to show the T&C text we need to know whether we're in the Google One Tap flow. We tried getting this information from the Authorisation state cookie (https://github.com/guardian/gateway/blob/02e793e227253ce4df6e1da79ac8b0dc04d00b58/src/server/lib/okta/openid-connect.ts#L326) inside the already existing `welcome/google` route but at this point of the request it has already been unset.

--- src/client/pages/WelcomeSocialPage.tsx
+++ src/client/pages/WelcomeSocialPage.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import useClientState from '@/client/lib/hooks/useClientState';
 import { WelcomeSocial } from '@/client/pages/WelcomeSocial';
 
-export const WelcomeSocialPage = () => {
+type WelcomeSocialPageProps = {
+	isGoogleOneTap?: boolean;
+};
+
+export const WelcomeSocialPage = ({
+	isGoogleOneTap,
+}: WelcomeSocialPageProps) => {
 	const clientState = useClientState();
 	const { pageData = {}, queryParams, shortRequestId } = clientState;
 	const { formError } = pageData;
