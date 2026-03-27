74497a6c1392208269e0c20cf284525180330adf
AshCorr <ashcorr20@gmail.com>
2026-03-27 10:52:43 +0000

    chore: Eslint enable `import/no-named-as-default`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -107,7 +107,6 @@ const config = tseslint.config(
 			'@typescript-eslint/prefer-nullish-coalescing': 'off',
 			'@typescript-eslint/no-unsafe-call': 'off',
 			'@typescript-eslint/no-unsafe-return': 'off',
-			'import/no-named-as-default': 'off',
 		},
 	},
 	{
