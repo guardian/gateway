b283bb31d9283f5e198ceb15c5aee4bedbdd6e92
AshCorr <ashcorr20@gmail.com>
2026-06-19 14:09:35 +0100

    feat: Allow specifying first and last name during registration/signin

--- src/server/routes/oauth.ts
+++ src/server/routes/oauth.ts
@@ -426,6 +426,16 @@ const authenticationHandler = async (
 			}
 		}
 
+		// Update the users first and last name if they exist in the authState
+		if (authState.data?.firstName || authState.data?.lastName) {
+			await updateUser(sub, {
+				profile: {
+					firstName: authState.data.firstName,
+					lastName: authState.data.lastName,
+				},
+			});
+		}
+
 		// clear any existing oauth application cookies if they exist
 		checkAndDeleteOAuthTokenCookies(req, res);
 
