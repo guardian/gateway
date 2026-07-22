3a52acdfa6fabb61aa7e67a1a2062d801823cec3
Jon Flynn <jon.flynn@guardian.co.uk>
2026-07-22 10:00:37 +0100

    Mock reCAPTCHA in sign-in e2e tests

--- playwright/tests/e2e/sign_in_2.spec.ts
+++ playwright/tests/e2e/sign_in_2.spec.ts
@@ -5,12 +5,17 @@ import {
 	updateOktaTestUserProfile,
 	closeCurrentOktaSession,
 } from '../../helpers/api/okta';
+import { mockClientRecaptcha } from '../../helpers/network/recaptcha';
 import { JOBS_TOS_URI } from '@/shared/model/Configuration';
 
 const returnUrl =
 	'https://www.theguardian.com/world/2013/jun/09/edward-snowden-nsa-whistleblower-surveillance';
 
 test.describe('Sign in flow, Okta enabled - split 2', () => {
+	test.beforeEach(async ({ page }) => {
+		await mockClientRecaptcha(page);
+	});
+
 	test.describe('Social sign in', () => {
 		test('redirects correctly for social sign in', async ({ page }) => {
 			await page.goto(`/signin?returnUrl=${encodeURIComponent(returnUrl)}`);
