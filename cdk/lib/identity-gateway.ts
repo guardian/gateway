70e2cf5796d12a7831060ef5c732c752ca76d5a8
Bruno Catarino <bruno.catarino@guardian.co.uk>
2026-09-07 15:33:38 +0100

    Add the AWS account too

--- cdk/lib/identity-gateway.ts
+++ cdk/lib/identity-gateway.ts
@@ -557,7 +557,7 @@ systemctl start ${app}
 				assumedBy: new WebIdentityPrincipal(`arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`,
 					{
 						StringLike: {
-							'token.actions.githubusercontent.com:sub': 'repo:guardian@164318/gateway:*',
+							'token.actions.githubusercontent.com:sub': 'repo:guardian@164318/gateway@226145935:*',
 						}
 					}
 				),
