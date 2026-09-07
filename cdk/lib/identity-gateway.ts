1a87017f7c3b6933589270b34611f411af88633d
Bruno Catarino <bruno.catarino@guardian.co.uk>
2026-09-07 15:12:24 +0100

    Update test snapshot as well to pass test.

--- cdk/lib/identity-gateway.ts
+++ cdk/lib/identity-gateway.ts
@@ -557,10 +557,7 @@ systemctl start ${app}
 				assumedBy: new WebIdentityPrincipal(`arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`,
 					{
 						StringLike: {
-							'token.actions.githubusercontent.com:sub': [
-								'repo:guardian/gateway:*',
-								'repo:guardian@164318/gateway:*'
-							]
+							'token.actions.githubusercontent.com:sub': 'repo:guardian@164318/gateway:*',
 						}
 					}
 				),
