0dad5fab2122b6e8271ac3d935461be5b5c98d91
Ioanna Kokkini <ioannakok@hotmail.com>
2026-09-18 13:15:40 +0100

    Disable dark mode stories for OnboardingPage

--- src/client/pages/OnboardingPage.stories.tsx
+++ src/client/pages/OnboardingPage.stories.tsx
@@ -4,6 +4,12 @@ import { OnboardingPage } from './OnboardingPage';
 export default {
 	title: 'Pages/OnboardingPage',
 	component: OnboardingPage,
+	chromatic: {
+		modes: {
+			'dark desktop': { disable: true },
+			'dark mobile': { disable: true },
+		},
+	},
 };
 
 export const Defaults = () => <OnboardingPage />;
